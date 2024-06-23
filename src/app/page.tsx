89aa897f8617873b70eccd99723d9174dff05c52
msdio <rlagofka1@gmail.com>
2024-06-23 20:29:10 +0900

    ✨ feat(custom): 디자인 시스템 폰트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-state-caution w-11 h-11" />
+
+      <p className="font-title-16">폰트 테스트</p>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss'
 
 import customColors from './src/styles/colors'
+import { PluginAPI } from 'tailwindcss/types/config'
+import typos from './src/styles/typographys'
 
 const config: Config = {
   content: [
@@ -15,8 +17,31 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        pretendard: [
+          'var(--font-pretendard)',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'system-ui',
+          'Roboto',
+          'Helvetica Neue',
+          'Segoe UI',
+          'Apple SD Gothic Neo',
+          'Noto Sans KR',
+          'Malgun Gothic',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'sans-serif',
+        ],
+      },
     },
     colors: customColors,
   },
+  plugins: [
+    ({ addUtilities }: { addUtilities: PluginAPI['addUtilities'] }) => {
+      addUtilities(typos)
+    },
+  ],
 }
 export default config
