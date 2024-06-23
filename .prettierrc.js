6ca4d6d58933a62bbee7973800f10361b76b76cd
msdio <rlagofka1@gmail.com>
2024-06-23 22:05:37 +0900

    ✨ feat(custom): prettier 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,13 @@
+/** @type {import('prettier').Config} */
+
 export default {
   semi: true,
-}
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,47 +1,47 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-import customColors from './src/styles/colors'
-import { PluginAPI } from 'tailwindcss/types/config'
-import typos from './src/styles/typographys'
+import customColors from "./src/styles/colors";
+import { PluginAPI } from "tailwindcss/types/config";
+import typos from "./src/styles/typographys";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
         pretendard: [
-          'var(--font-pretendard)',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'system-ui',
-          'Roboto',
-          'Helvetica Neue',
-          'Segoe UI',
-          'Apple SD Gothic Neo',
-          'Noto Sans KR',
-          'Malgun Gothic',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'sans-serif',
+          "var(--font-pretendard)",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "system-ui",
+          "Roboto",
+          "Helvetica Neue",
+          "Segoe UI",
+          "Apple SD Gothic Neo",
+          "Noto Sans KR",
+          "Malgun Gothic",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "sans-serif",
         ],
       },
     },
     colors: customColors,
   },
   plugins: [
-    ({ addUtilities }: { addUtilities: PluginAPI['addUtilities'] }) => {
-      addUtilities(typos)
+    ({ addUtilities }: { addUtilities: PluginAPI["addUtilities"] }) => {
+      addUtilities(typos);
     },
   ],
-}
-export default config
+};
+export default config;
