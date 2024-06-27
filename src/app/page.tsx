fcb64e0c85521ecb6ac99edf3cc00d01ff7c8f2b
MeaninGood <jsmeaningood@gmail.com>
2024-06-28 00:37:22 +0900

    🔧 add(custom): app/page.tsx에 아이콘 예시코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import StarIcon from '@/components/icons/StarIcon';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-state-caution w-11 h-11" />
 
+      <StarIcon width={100} height={100} fill="red" stroke="blue" />
+      <StarIcon size={20} />
       <p className="font-title-16">폰트 테스트</p>
       <p className="font-designer">designer font test 012012</p>
     </main>
