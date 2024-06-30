8309f33e33df1979ddee0ceb40c910258b788b8e
MeaninGood <jsmeaningood@gmail.com>
2024-06-30 23:38:37 +0900

    ✨ feat(custom): toggle switch 개발

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import Alert from '@/components/modal/Alert';
+import ToggleSwitch from '@/components/toggle-switch/ToggleSwitch';
 
 export default function Home() {
   const [open, setOpen] = useState(false);
@@ -10,6 +11,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-state-caution w-11 h-11" />
+
+      <ToggleSwitch />
 
       <p className="font-title-16">폰트 테스트</p>
       <p className="font-designer">designer font test 012012</p>
