3d51391d689f079184af4a2ef166e50a2403456c
MeaninGood <jsmeaningood@gmail.com>
2024-06-30 13:37:28 +0900

    🎨 css(custom): toastContainer 화면 상단으로 변경, opacity100 -\> 1로 변경

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -12,7 +12,7 @@ const ToastContainer = () => {
 
   return toasts.length > 0
     ? createPortal(
-        <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 space-y-3 z-50">
+        <div className="fixed top-5 left-1/2 transform -translate-x-1/2 space-y-3 z-50">
           {toasts.map((toast) => (
             <Toast key={toast.id} message={toast.message} id={toast.id} iconType={toast.iconType} />
           ))}
