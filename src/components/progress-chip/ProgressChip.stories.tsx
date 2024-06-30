b5ffc51707715ec9c4d562e2bd1083816a6d6e6c
msdio <rlagofka1@gmail.com>
2024-06-30 13:55:04 +0900

    🎨 css(custom): progress chip 스토리북 arg type 수정

--- src/components/progress-chip/ProgressChip.stories.tsx
+++ src/components/progress-chip/ProgressChip.stories.tsx
@@ -3,14 +3,23 @@ import { Meta, StoryObj } from '@storybook/react';
 import ProgressChip from './ProgressChip';
 
 const meta = {
-  title: 'Components',
+  title: 'Components/Progress',
   component: ProgressChip,
+  argTypes: {
+    percentage: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+      },
+    },
+  },
 } satisfies Meta<typeof ProgressChip>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Progress: Story = {
+export const Chip: Story = {
   args: {
     percentage: 30,
   },
