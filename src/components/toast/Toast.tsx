0c8bd81247b361e73a5ac613b9ab5b43c0bb0067
MeaninGood <jsmeaningood@gmail.com>
2024-07-05 23:29:43 +0900

    🎨 css(custom): show opacity 100 -\> 1로 수정

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -30,7 +30,7 @@ const Toast = ({ id, message, iconType }: ToastProps) => {
       id={String(id)}
       role="alert"
       className={`mx-auto min-w-[24.125rem] max-w-[36.625rem] h-12 bg-surface-alternative text-text-invert font-body-16 flex items-center px-4 gap-2 rounded-lg transition-transform transition-opacity duration-300 ease-out ${
-        show ? 'translate-y-0 opacity-100' : '-translate-y-4 opacity-0'
+        show ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0'
       }`}
     >
       {iconType && ToastIcon[iconType]}
