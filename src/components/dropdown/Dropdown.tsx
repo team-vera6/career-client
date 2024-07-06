932beb12d8155342f1d14af8828f8bc11d34fa3c
MeaninGood <jsmeaningood@gmail.com>
2024-07-06 16:47:36 +0900

    🎨 css(custom): li bg css 수정

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -9,11 +9,12 @@ interface DropdownItem {
 }
 
 interface Props {
+  id: string;
   initialItem?: DropdownItem;
   items: Array<DropdownItem>;
 }
 
-const Dropdown = ({ initialItem = { name: '선택하세요', value: '' }, items }: Props) => {
+const Dropdown = ({ id, initialItem = { name: '선택하세요', value: '' }, items }: Props) => {
   const [showOptions, setShowOptions] = useState(false);
   const [selectedItem, setSelectedItem] = useState(initialItem);
 
@@ -25,13 +26,13 @@ const Dropdown = ({ initialItem = { name: '선택하세요', value: '' }, items 
   return (
     <div className="relative w-[15.188rem] h-[2.75rem]">
       <button
-        name="select"
-        id="select-id"
-        className="appearance-none w-full h-full border border-line-normal rounded-lg p-3 font-body-14 cursor-pointer
+        type="button"
+        id={id}
+        className="appearance-none w-full h-full p-3 font-body-14 cursor-pointer
+        border border-line-normal rounded-lg
         flex items-center justify-between
-        focus:outline-none focus:border-line-focus hover:border-line-normalHover disabled:bg-surface-base
-        disabled:cursor-not-allowed disabled:hover:border-line-normal relative
-        "
+        focus:outline-none focus:border-line-focus hover:border-line-normalHover
+        disabled:bg-surface-base disabled:cursor-not-allowed disabled:hover:border-line-normal"
         onClick={() => setShowOptions((prev) => !prev)}
       >
         <span>{selectedItem.name}</span>
@@ -39,14 +40,15 @@ const Dropdown = ({ initialItem = { name: '선택하세요', value: '' }, items 
       </button>
       {showOptions && (
         <ul
-          className="absolute w-full top-[2.75rem] left-0 border border-line-assistive rounded-lg py-1 bg-surface-foreground z-10 cursor-pointer"
+          className="absolute w-full top-[2.75rem] left-0 py-1 z-10 cursor-pointer
+          border border-line-assistive rounded-lg  bg-surface-foreground"
           style={{
             boxShadow: '0px 8px 20px 0px #00000014, 0px 0px 2px 0px #0000000D',
           }}
         >
           {items.map((item) => (
             <li
-              className="h-[2.75rem] py-3 px-5 hover:bg-gray-100"
+              className="h-[2.75rem] py-3 px-5 hover:bg-surface-background"
               key={item.value}
               value={item.value}
               onClick={() => onClickItem(item)}
