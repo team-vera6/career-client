474e5f4ed1ca578f3e1fdea0133801abef605cdf
msdio <rlagofka1@gmail.com>
2024-07-06 17:10:59 +0900

    ✨ feat(custom): date picker 컴포넌트 제작

---
+++ src/components/date-picker/DatePicker.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+'use client';
+
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import './date-picker.css';
+
+import { addWeeks } from 'date-fns';
+import { ko } from 'date-fns/locale';
+import { useState } from 'react';
+import { DateRange, Range } from 'react-date-range';
+
+import colors from '@/styles/colors';
+
+const DatePicker = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [date, setDate] = useState<Range[]>([
+    {
+      startDate: new Date(),
+      endDate: addWeeks(new Date(), 1),
+      key: 'selection',
+    },
+  ]);
+
+  return (
+    <DateRange
+      onChange={(item) => setDate([item.selection])}
+      direction="horizontal"
+      months={2}
+      locale={ko}
+      ranges={date}
+      color={colors.surface.background}
+      monthDisplayFormat="yyyy년 MMM"
+      showDateDisplay={false}
+      showMonthAndYearPickers={false}
+      moveRangeOnFirstSelection={false}
+      fixedHeight
+      showPreview={false}
+    />
+  );
+};
+
+export default DatePicker;
