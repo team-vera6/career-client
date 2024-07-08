3541dc8698a9cf81a311aa7815ec07827eaa613b
msdio <dev.msdio@gmail.com>
2024-07-08 21:26:07 +0900

    🪛 fix(custom): merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import TextEditor from '@/components/text-editor/TextEditor';
-
 export default function Home() {
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TextEditor />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="bg-state-caution w-11 h-11" />
     </main>
   );
 }
