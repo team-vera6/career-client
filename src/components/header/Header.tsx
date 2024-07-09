fa06a6e8a26da1c7e5fe1ad1b234ffd92c595445
msdio <dev.msdio@gmail.com>
2024-07-09 17:23:38 +0900

    🎨 css(custom): 헤더 높이 및 간격 조정

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import TextLogo from '@/components/icons/TextLogo';
+
+const Header = () => {
+  return (
+    <header className="h-[78px] w-full fixed top-0 left-0 right-0 bg-surface-invert flex items-center justify-center">
+      <div className="w-full max-w-[1696px] flex items-center justify-between px-12">
+        <TextLogo />
+
+        <div className="flex items-center gap-5">
+          <div className="w-24 h-12 bg-red-10 flex items-center justify-center rounded-full">
+            0/4
+          </div>
+          <div className="w-10 h-10 bg-surface-alternative rounded-[14px] flex items-center justify-center text-text-invert">
+            이
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
