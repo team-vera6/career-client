1f99fb3097b7fbb133d7625eb5d8f0689943f503
msdio <dev.msdio@gmail.com>
2024-07-09 10:39:39 +0900

    🎨 css(custom): header에 max width 적용

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,17 @@ import TextLogo from '@/components/icons/TextLogo';
 
 const Header = () => {
   return (
-    <header className="h-[4.875rem] w-full sticky top-0 left-0 right-0 bg-surface-invert flex items-center justify-between px-12">
-      <TextLogo />
+    <header className="h-[4.875rem] w-full sticky top-0 left-0 right-0 bg-surface-invert flex items-center justify-center">
+      <div className="w-full max-w-[1696px] flex items-center justify-between px-12">
+        <TextLogo />
 
-      <div className="flex items-center gap-5">
-        <div className="w-24 h-12 bg-red-10 flex items-center justify-center rounded-full">0/4</div>
-        <div className="w-10 h-10 bg-surface-alternative rounded-[14px] flex items-center justify-center text-text-invert">
-          이
+        <div className="flex items-center gap-5">
+          <div className="w-24 h-12 bg-red-10 flex items-center justify-center rounded-full">
+            0/4
+          </div>
+          <div className="w-10 h-10 bg-surface-alternative rounded-[14px] flex items-center justify-center text-text-invert">
+            이
+          </div>
         </div>
       </div>
     </header>
