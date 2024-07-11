1a8699604317c5aa1714b02a2e2a2a7fc9f5b170
msdio <dev.msdio@gmail.com>
2024-07-12 01:34:30 +0900

    🎨 css(custom): 마크다운 메뉴 아이콘 적용

--- src/components/text-editor/MenuBar.tsx
+++ src/components/text-editor/MenuBar.tsx
@@ -1,5 +1,15 @@
 import { Editor } from '@tiptap/react';
 
+import BoldIcon from '../icons/markdown/BoldIcon';
+import CodeBlockIcon from '../icons/markdown/CodeBlockIcon';
+import Header1Icon from '../icons/markdown/Header1Icon';
+import Header2Icon from '../icons/markdown/Header2Icon';
+import Header3Icon from '../icons/markdown/Header3Icon';
+import ItalicIcon from '../icons/markdown/ItalicIcon';
+import OrderedListIcon from '../icons/markdown/OrderedListIcon';
+import UnderlineIcon from '../icons/markdown/UnderlineIcon';
+import UnorderedListIcon from '../icons/markdown/UnorderedListIcon';
+
 interface Props {
   editor: Editor | null;
 }
@@ -12,40 +22,40 @@ const MenuBar = ({ editor }: Props) => {
   return (
     <section className="flex items-center gap-2 h-[3.875rem] border-b border-line-assistive">
       <button className="w-6 h-6" onClick={() => editor.chain().focus().toggleBold().run()}>
-        B
+        <BoldIcon size={20} />
       </button>
       <button className="w-6 h-6" onClick={() => editor.chain().focus().toggleItalic().run()}>
-        I
+        <ItalicIcon size={20} />
       </button>
       <button className="w-6 h-6" onClick={() => editor.chain().focus().toggleStrike().run()}>
-        U
+        <UnderlineIcon size={20} />
       </button>
       <button className="w-6 h-6" onClick={() => editor.chain().focus().toggleCode().run()}>
-        {'</>'}
+        <CodeBlockIcon size={20} />
       </button>
       <button
         className="w-6 h-6"
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
       >
-        H1
+        <Header1Icon size={20} />
       </button>
       <button
         className="w-6 h-6"
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
       >
-        H2
+        <Header2Icon size={20} />
       </button>
       <button
         className="w-6 h-6"
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
       >
-        H3
+        <Header3Icon size={20} />
       </button>
       <button className="w-6 h-6" onClick={() => editor.chain().focus().toggleOrderedList().run()}>
-        123
+        <OrderedListIcon size={20} />
       </button>
       <button className="w-6 h-6" onClick={() => editor.chain().focus().toggleBulletList().run()}>
-        bul
+        <UnorderedListIcon size={20} />
       </button>
     </section>
   );
