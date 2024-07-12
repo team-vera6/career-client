b9abf72669549f1edd468c0bdf64078d27050e24
msdio <dev.msdio@gmail.com>
2024-07-12 10:08:02 +0900

    🎨 css(custom): checkbox input의 버튼들 hover시 나타날 수 있도록 변경

--- src/components/inputs/checkbox/CheckboxInput.tsx
+++ src/components/inputs/checkbox/CheckboxInput.tsx
@@ -1,27 +1,35 @@
-import { ReactNode } from 'react';
+'use client';
+
+import { memo, ReactNode, useState } from 'react';
 
 import RectangleCheckIcon from '@/components/icons/RectangleCheckIcon';
 import colors from '@/styles/colors';
 
 interface Props {
   value: string;
-  onChange: (value: string) => void;
   checked: boolean;
-  onClickCheckbox: () => void;
+  onChange?: (value: string) => void;
+  onClickCheckbox?: () => void;
   width?: string;
   buttons?: ReactNode;
   disabled?: boolean;
+  showButtonsOnHover?: boolean;
 }
 
 const CheckboxInput = ({
   value,
-  onChange,
   checked,
+  onChange,
   onClickCheckbox,
   width,
   buttons,
   disabled,
+  showButtonsOnHover = false,
 }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const showRightButtons = !showButtonsOnHover || (showButtonsOnHover && isHovered);
+
   return (
     <div
       className="px-3 py-3.5 flex items-center justify-between rounded-lg h-12 focus-within:outline focus-within:outline-1 focus-within:outline-text-primary bg-surface-foreground"
@@ -31,6 +39,8 @@ const CheckboxInput = ({
         backgroundColor: disabled ? colors.surface.assistive : '',
         opacity: disabled ? 0.8 : 1,
       }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <div className="flex items-center gap-2">
         <div onClick={onClickCheckbox}>
@@ -41,16 +51,21 @@ const CheckboxInput = ({
           className="w-full font-body-16 text-text-strong outline-none bg-transparent"
           disabled={disabled}
           value={value}
-          onChange={(e) => onChange(e.currentTarget.value)}
+          onChange={(e) => onChange?.(e.currentTarget.value)}
         />
       </div>
 
-      {buttons}
+      <div
+        className="transition-all duration-200 flex items-center"
+        style={isHovered ? { opacity: 1 } : { opacity: 0 }}
+      >
+        {showRightButtons && buttons}
+      </div>
     </div>
   );
 };
 
-export default CheckboxInput;
+export default memo(CheckboxInput);
 
 const UnCheckedIcon = () => (
   <div className="w-5 h-5 rounded border-[1.5px] border-text-assistive bg-surface-foreground" />
