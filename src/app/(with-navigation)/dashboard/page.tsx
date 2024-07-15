93c3e2b6c1fee01c5d0b042931871d153fa53368
msdio <dev.msdio@gmail.com>
2024-07-16 00:48:45 +0900

    ♻️ rfac(custom): client 컴포넌트 분리

--- src/app/(with-navigation)/dashboard/page.tsx
+++ src/app/(with-navigation)/dashboard/page.tsx
@@ -1,33 +1,11 @@
-'use client';
-
-import Image from 'next/image';
-import { useState } from 'react';
-
-import EmptyMemoImage from '@/assets/images/memo-empty.png';
-import EmptyTodoImage from '@/assets/images/todo-empty.png';
 import FireIcon from '@/components/icons/FireIcon';
 import FlagIcon from '@/components/icons/FlagIcon';
-import PlusIcon from '@/components/icons/PlusIcon';
 import YellowFolderIcon from '@/components/icons/YellowFolderIcon';
 
-export default function DashboardPage() {
-  const [todo, setTodo] = useState({
-    text: '',
-    checked: false,
-  });
-  const [todo2, setTodo2] = useState({
-    text: '',
-    checked: false,
-  });
-  const [todo3, setTodo3] = useState({
-    text: '',
-    checked: false,
-  });
-  const [todo4, setTodo4] = useState({
-    text: '',
-    checked: false,
-  });
+import MemoListComponents from './MemoListComponents';
+import TodoList from './TodoListComponents';
 
+export default function DashboardPage() {
   return (
     <div>
       <section className="flex gap-3 w-full mb-7">
@@ -59,101 +37,8 @@ export default function DashboardPage() {
         </section>
 
         <div className="flex gap-9">
-          {/* 이번 주 할 일 */}
-          <section className="w-full">
-            <div className="flex items-center justify-between w-full">
-              <p className="font-title-16 text-text-strong">이번 주 할 일</p>
-              <button className="button-line button-small">
-                <PlusIcon size={20} />
-                <p className="font-body-13 text-text-strong">추가</p>
-              </button>
-            </div>
-
-            {/* <div className="flex flex-col gap-3 mt-4">
-              <CheckboxInput
-                value={todo.text}
-                checked={todo.checked}
-                onChange={(value) => setTodo((prev) => ({ ...prev, text: value }))}
-                onClickCheckbox={() => setTodo((prev) => ({ ...prev, checked: !prev.checked }))}
-                showButtonsOnHover
-                buttons={
-                  <button>
-                    <DeleteIcon size={20} />
-                  </button>
-                }
-              />
-              <CheckboxInput
-                value={todo2.text}
-                checked={todo2.checked}
-                onChange={(value) => setTodo2((prev) => ({ ...prev, text: value }))}
-                onClickCheckbox={() => setTodo2((prev) => ({ ...prev, checked: !prev.checked }))}
-                showButtonsOnHover
-                buttons={
-                  <button>
-                    <DeleteIcon size={20} />
-                  </button>
-                }
-              />
-              <CheckboxInput
-                value={todo3.text}
-                checked={todo3.checked}
-                onChange={(value) => setTodo3((prev) => ({ ...prev, text: value }))}
-                onClickCheckbox={() => setTodo3((prev) => ({ ...prev, checked: !prev.checked }))}
-                showButtonsOnHover
-                buttons={
-                  <button>
-                    <DeleteIcon size={20} />
-                  </button>
-                }
-              />
-              <CheckboxInput
-                value={todo4.text}
-                checked={todo4.checked}
-                onChange={(value) => setTodo4((prev) => ({ ...prev, text: value }))}
-                onClickCheckbox={() => setTodo4((prev) => ({ ...prev, checked: !prev.checked }))}
-                showButtonsOnHover
-                disabled
-                buttons={
-                  <button>
-                    <DeleteIcon size={20} />
-                  </button>
-                }
-              />
-            </div> */}
-
-            <div className="w-full flex items-center flex-col gap-5 mt-[3.75rem]">
-              <p className="font-body-16 text-text-normal">작성한 할 일이 없어요</p>
-              <Image src={EmptyTodoImage} width={100} height={100} alt="no memo" />
-            </div>
-          </section>
-
-          {/* 이번 주 메모 */}
-          <section className="shrink-0 min-w-[252px]">
-            <div className="flex items-center justify-between w-full">
-              <p className="font-title-16 text-text-strong">이번 주 메모</p>
-              <button className="button-line button-small">
-                <PlusIcon size={20} />
-                <p className="font-body-13 text-text-strong">추가</p>
-              </button>
-            </div>
-
-            {/* <div className="flex flex-col gap-3 mt-4">
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-              <Memo title="로열티 기획" memo="회원 등급, 포인트 시스템" date="7.12" />
-            </div> */}
-            <div className="w-[15.75rem] flex items-center flex-col gap-5 mt-[3.75rem]">
-              <p className="font-body-16 text-text-normal">남긴 메모가 없어요</p>
-              <Image src={EmptyMemoImage} width={100} height={100} alt="no memo" />
-            </div>
-          </section>
+          <TodoList />
+          <MemoListComponents />
         </div>
       </div>
     </div>
