836e638ed5e256bd9c4ca2cdf54efeb3205ed0d3
MeaninGood <jsmeaningood@gmail.com>
2024-07-15 21:53:14 +0900

    ♻️ rfac(custom): section 태그 변경

--- src/app/review/layout.tsx
+++ src/app/review/layout.tsx
@@ -7,12 +7,10 @@ export default function ReviewLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section>
-      <div className="w-full h-full px-12 max-w-[1696px] mx-auto pt-[3.75rem] relative">
-        <LeftNavigationBar />
-        <main>{children}</main>
-        <ToastContainer />
-      </div>
-    </section>
+    <div className="w-full h-full px-12 max-w-[1696px] mx-auto pt-[3.75rem] relative">
+      <LeftNavigationBar />
+      <main>{children}</main>
+      <ToastContainer />
+    </div>
   );
 }
