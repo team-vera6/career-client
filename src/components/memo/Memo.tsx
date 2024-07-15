1461bab26cb96463f20ec7ef78492a2264795222
MeaninGood <jsmeaningood@gmail.com>
2024-07-15 22:58:50 +0900

    ♻️ rfac(custom): memo 컴포넌트 background 추가

--- src/components/memo/Memo.tsx
+++ src/components/memo/Memo.tsx
@@ -10,7 +10,7 @@ const Memo = ({ isBookmark = false, memo, date }: Props) => {
   return (
     <div
       className="w-[15.75rem] h-[8.75rem] rounded-lg pt-5 pr-4 pb-2.5 pl-5
-      flex flex-col gap-2.5"
+      flex flex-col gap-2.5 bg-surface-foreground"
       style={{
         boxShadow: '0px 8px 20px 0px #00000014, 0px 0px 2px 0px #0000000D',
       }}
