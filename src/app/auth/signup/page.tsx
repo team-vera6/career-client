aa977f0e99b1ac40cdc31f11f2af4c19a298a4e5
jiseonjang <jsmeaningood@gmail.com>
2024-07-15 15:22:09 +0900

    ✨ feat(custom): 회원가입 페이지 개발

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,14 @@
+import SignupComponents from './components';
+
+const SignUpPage = () => {
+  return (
+    <div className="w-[25rem] flex flex-col items-center pt-[3.75rem]">
+      <p className="font-body-14 mb-12 text-text-strong">
+        사용 중이신 이메일 주소로 가입할 수 있어요.
+      </p>
+      <SignupComponents />
+    </div>
+  );
+};
+
+export default SignUpPage;
