927e483d62adc935e4bd744c964dce2922865fe6
jiseonjang <jsmeaningood@gmail.com>
2024-07-16 12:50:14 +0900

    ♻️ rfac(custom): 페이지 구조 수정 및 태그 수정

--- src/app/review/(components)/WeeklyMemo.tsx
+++ src/app/review/(components)/WeeklyMemo.tsx
@@ -2,15 +2,12 @@ import Memo from '@/components/memo/Memo';
 
 const WeeklyMemo = () => {
   return (
-    <div className="flex flex-col gap-[2.125rem]">
-      <p className="font-title-16">이번주 작성한 메모</p>
-      <div className="flex flex-col gap-3">
-        <Memo isBookmark memo="로열티 기획" date="7.12" />
-        <Memo isBookmark memo="로열티 기획" date="7.12" />
-        <Memo memo="로열티 기획" date="7.12" />
-        <Memo isBookmark memo="로열티 기획" date="7.12" />
-        <Memo memo="로열티 기획" date="7.12" />
-      </div>
+    <div className="flex flex-col gap-3">
+      <Memo isBookmark memo="로열티 기획" date="7.12" />
+      <Memo isBookmark memo="로열티 기획" date="7.12" />
+      <Memo memo="로열티 기획" date="7.12" />
+      <Memo isBookmark memo="로열티 기획" date="7.12" />
+      <Memo memo="로열티 기획" date="7.12" />
     </div>
   );
 };
--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -2,8 +2,13 @@ import WeeklyMemo from './(components)/WeeklyMemo';
 
 export default function ReviewPage() {
   return (
-    <div className="w-100 h-full pt-[3.75rem] flex gap-[13.5rem]">
-      <WeeklyMemo />
+    <div className="w-full h-full pt-[3.75rem] flex">
+      <aside className="flex flex-col gap-[2.125rem]">
+        <p className="font-title-16">이번주 작성한 메모</p>
+        <WeeklyMemo />
+      </aside>
+
+      <div className="h-full w-[44.25rem] flex flex-col mx-auto">컨텐츠</div>
     </div>
   );
 }
