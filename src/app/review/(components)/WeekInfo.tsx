e888ffa03e61adbd5c32defcbbc6c450e427720b
jiseonjang <jsmeaningood@gmail.com>
2024-07-16 17:46:45 +0900

    1페이지 개발중

--- src/app/review/(components)/WeekInfo.tsx
+++ src/app/review/(components)/WeekInfo.tsx
@@ -2,30 +2,42 @@
 
 import { useState } from 'react';
 
+import PlusIcon from '@/components/icons/PlusIcon';
 import ScorePicker from '@/components/score-picker/ScorePicker';
 
-const WeekInfo = () => {
-  const [selectedScore, setSelectedScore] = useState(0);
+export const CurrentWeek = () => {
   return (
-    <section className="w-full flex flex-col gap-6">
-      <div className="flex justify-between items-center">
-        <p className="font-head-24">
-          6월 1주차<sup className="font-body-14 ml-2">6/3 ~ 6/7</sup>
-        </p>
-        점 3개
-      </div>
-      <div className="flex flex-col gap-4">
-        <p className="font-head-20">6월 1주차는 어떠셨나요?</p>
-        <div className="h-36 bg-surface-foreground rounded-2xl flex items-center justify-center flex-col gap-4">
-          <div className="flex justify-between w-[26.25rem] font-title-14">
-            <p>아쉬워요</p>
-            <p>만족해요</p>
-          </div>
-          <ScorePicker score={selectedScore} setScore={(e) => setSelectedScore(e)} />
-        </div>
-      </div>
-    </section>
+    <>
+      <p className="font-head-24">
+        6월 1주차<sup className="font-body-14 ml-2">6/3 ~ 6/7</sup>
+      </p>
+      점 3개
+    </>
   );
 };
 
-export default WeekInfo;
+export const CurrentWeekScore = () => {
+  const [selectedScore, setSelectedScore] = useState(0);
+
+  return (
+    <>
+      <p className="font-head-20">6월 1주차는 어떠셨나요?</p>
+      <div className="h-36 bg-surface-foreground rounded-2xl flex items-center justify-center flex-col gap-4">
+        <div className="flex justify-between w-[26.25rem] font-title-14">
+          <p>아쉬워요</p>
+          <p>만족해요</p>
+        </div>
+        <ScorePicker score={selectedScore} setScore={(e) => setSelectedScore(e)} />
+      </div>
+    </>
+  );
+};
+
+export const AddTodo = ({ week }: { week: 'current' | 'next' }) => {
+  return (
+    <button className="button-text button-small" onClick={() => console.log(week)}>
+      <PlusIcon />
+      <p className="font-body-13">추가</p>
+    </button>
+  );
+};
