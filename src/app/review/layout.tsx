45c234655c62027815d8cc4a2ac8ad3aa9220326
jiseonjang <jsmeaningood@gmail.com>
2024-07-17 18:06:17 +0900

    ♻️ rfac(custom): 레이아웃 수정, page 이동 및 분리한 컴포넌트 적용

--- src/app/review/layout.tsx
+++ src/app/review/layout.tsx
@@ -1,5 +1,7 @@
 import TextLogo from '@/components/icons/TextLogo';
 
+import WeeklyMemo from './_components/weekly-memo/WeeklyMemo';
+
 export default function ReviewLayout({
   children,
 }: Readonly<{
@@ -10,7 +12,15 @@ export default function ReviewLayout({
       <header>
         <TextLogo />
       </header>
-      <main>{children}</main>
+
+      <div className="w-full h-full pt-[3.75rem] flex">
+        <aside className="flex flex-col gap-[2.125rem]">
+          <p className="font-title-16">이번주 작성한 메모</p>
+          <WeeklyMemo />
+        </aside>
+
+        <main className="h-full w-[44.25rem] mx-auto">{children}</main>
+      </div>
     </div>
   );
 }
