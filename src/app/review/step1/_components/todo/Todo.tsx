d131028476346f580b2a3b371317fc427e57088f
jiseonjang <jsmeaningood@gmail.com>
2024-07-18 16:57:01 +0900

    ✨ feat(custom): 이번주 / 다음주 할일 컴포너트 개발

--- src/app/review/step1/_components/todo/Todo.tsx
+++ src/app/review/step1/_components/todo/Todo.tsx
@@ -1,11 +1,24 @@
+import { AddButton } from '@/app/review/_components/add-button/AddButton';
+
+import { TodoList } from './TodoList';
+
 export const Todo = () => {
   return (
     <div className="rounded-2xl bg-surface-foreground p-9 flex flex-col gap-7">
       <div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-3">
           <p className="font-title-16">이번주 할 일</p>
-          {/* <AddTodo week="current" /> */}
+          <AddButton category="currentTodo" />
         </div>
+        <TodoList week="current" />
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between mb-3">
+          <p className="font-title-16">다음주 할 일</p>
+          <AddButton category="nextTodo" />
+        </div>
+        <TodoList week="next" />
       </div>
     </div>
   );
