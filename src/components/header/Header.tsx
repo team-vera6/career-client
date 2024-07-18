a28cf24f60f5f88731585bb711de3e1f49aa6efc
msdio <dev.msdio@gmail.com>
2024-07-18 14:51:46 +0900

    ✨ feat(custom): 프로필 설정 모달 제작

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,14 @@
+'use client';
+
+import { useState } from 'react';
+
 import TextLogo from '@/components/icons/TextLogo';
 
+import SettingProfileModal from '../modal/setting-profile';
+
 const Header = () => {
+  const [openSettingModal, setOpenSettingModal] = useState(false);
+
   return (
     <header className="h-[4.875rem] w-full sticky top-0 left-0 right-0 bg-surface-invert flex items-center justify-center z-10">
       <div className="w-full max-w-[1696px] flex items-center justify-between px-12">
@@ -10,11 +18,16 @@ const Header = () => {
           <div className="w-24 h-12 bg-red-10 flex items-center justify-center rounded-full">
             0/4
           </div>
-          <div className="w-10 h-10 bg-surface-alternative rounded-[14px] flex items-center justify-center text-text-invert">
+          <div
+            className="w-10 h-10 bg-surface-alternative rounded-[14px] flex items-center justify-center text-text-invert select-none"
+            onClick={() => setOpenSettingModal(true)}
+          >
             이
           </div>
         </div>
       </div>
+
+      <SettingProfileModal isOpen={openSettingModal} onDismiss={() => setOpenSettingModal(false)} />
     </header>
   );
 };
---
+++ src/components/modal/setting-profile/index.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useState } from 'react';
+
+import Modal from '../Modal';
+import { ModalProps } from '../ModalContainer';
+
+import SettingAccount from './SettingAccount';
+import SettingProfile from './SettingProfile';
+
+const menus = [
+  {
+    label: '프로필',
+    component: <SettingProfile />,
+  },
+  {
+    label: '계정',
+    component: <SettingAccount />,
+  },
+];
+
+const SettingProfileModal = (props: ModalProps) => {
+  const [currentMenu, setCurrentMenu] = useState(menus[0].label);
+
+  return (
+    <Modal title="설정" closeIcon {...props}>
+      <div className="flex gap-[1.375rem] pl-3 pr-7 pt-1 pb-[3.75rem] w-[29rem]">
+        {/* left navigation bar */}
+        <div className="flex flex-col gap-1">
+          {menus.map((menu) => (
+            <button
+              key={menu.label}
+              className={
+                currentMenu === menu.label
+                  ? 'w-[4.625rem] h-8 flex items-center font-body-16 text-text-strong pl-3'
+                  : 'w-[4.625rem] h-8 flex items-center font-body-16 text-text-neutral pl-3'
+              }
+              onClick={() => setCurrentMenu(menu.label)}
+            >
+              {menu.label}
+            </button>
+          ))}
+        </div>
+
+        {menus.find((menu) => menu.label === currentMenu)?.component}
+      </div>
+    </Modal>
+  );
+};
+
+export default SettingProfileModal;
