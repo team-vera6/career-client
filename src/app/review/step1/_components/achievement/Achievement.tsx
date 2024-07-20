48a29156266c99338b2dc46481ef292fbdbac18b
MeaninGood <jsmeaningood@gmail.com>
2024-07-20 21:10:54 +0900

    🎨 css(custom): 스코어 폰트색 변경

--- src/app/review/step1/_components/achievement/Achievement.tsx
+++ src/app/review/step1/_components/achievement/Achievement.tsx
@@ -3,9 +3,9 @@ import { Score } from './Score';
 export const Achievement = () => {
   return (
     <div className="h-36 bg-surface-foreground rounded-2xl flex items-center justify-center flex-col gap-4">
-      <div className="flex justify-between w-[26.25rem">
-        <p className="font-title-14 text-text-strong">아쉬워요</p>
-        <p className="font-title-14 text-text-strong">만족해요</p>
+      <div className="flex justify-between w-[26.25rem] font-title-14 text-text-strong">
+        <p>아쉬워요</p>
+        <p>만족해요</p>
       </div>
       <Score />
     </div>
