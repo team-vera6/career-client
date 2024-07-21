2a0cd6c7b8777c983dd2bc2a500b136d7d940fcf
MeaninGood <jsmeaningood@gmail.com>
2024-07-21 20:37:01 +0900

    🪛 fix(custom): textarea display block 추가

--- src/components/inputs/textarea/Textarea.tsx
+++ src/components/inputs/textarea/Textarea.tsx
@@ -1,6 +1,5 @@
 import { HTMLAttributes, TextareaHTMLAttributes } from 'react';
 
-import colors from '@/styles/colors';
 import { cn } from '@/utils/tailwind';
 
 interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
@@ -13,21 +12,16 @@ const Textarea = ({ errorText, className, ...rest }: Props) => {
     <>
       <textarea
         className={cn(
-          'w-full p-3 border border-line-normal bg-surface-foreground rounded-lg font-body-14 text-neutral-90 hover:border-line-normalHover placeholder:text-text-normal focus:outline-line-focus disabled:border-line-normal disabled:text-text-assistive disabled:placeholder:text-text-assistive resize-none',
+          'block w-full p-3 border border-line-normal bg-surface-foreground rounded-lg font-body-14 text-neutral-90 hover:border-line-normalHover placeholder:text-text-normal focus:outline-line-focus disabled:border-line-normal disabled:text-text-assistive disabled:placeholder:text-text-assistive resize-none',
           className,
+          errorText && 'border-state-negative outline-state-negative',
         )}
-        style={
-          errorText
-            ? {
-                borderColor: colors.state.negative,
-                outline: colors.state.negative,
-              }
-            : {}
-        }
         {...rest}
       />
 
-      <p className="text-state-negative font-body-12 mt-[2px]">{errorText}</p>
+      {errorText && (
+        <p className="text-state-negative font-body-12 mt-[2px]">{errorText}</p>
+      )}
     </>
   );
 };
