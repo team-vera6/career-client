22829e3a52a8c2e5dc948525cff636a1b9009b12
MeaninGood <jsmeaningood@gmail.com>
2024-07-21 20:21:40 +0900

    🪛 fix(custom): textarea, ProgressChip css 수정

--- src/components/inputs/textarea/Textarea.tsx
+++ src/components/inputs/textarea/Textarea.tsx
@@ -1,16 +1,21 @@
-import { TextareaHTMLAttributes } from 'react';
+import { HTMLAttributes, TextareaHTMLAttributes } from 'react';
 
 import colors from '@/styles/colors';
+import { cn } from '@/utils/tailwind';
 
 interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   errorText?: string;
+  className?: HTMLAttributes<HTMLTextAreaElement>['className'];
 }
 
-const Textarea = ({ errorText, ...rest }: Props) => {
+const Textarea = ({ errorText, className, ...rest }: Props) => {
   return (
     <>
       <textarea
-        className="p-3 border border-line-normal bg-surface-foreground rounded-lg font-body-14 text-neutral-90 hover:border-line-normalHover placeholder:text-text-normal focus:outline-line-focus disabled:border-line-normal disabled:text-text-assistive disabled:placeholder:text-text-assistive resize-none"
+        className={cn(
+          'w-full p-3 border border-line-normal bg-surface-foreground rounded-lg font-body-14 text-neutral-90 hover:border-line-normalHover placeholder:text-text-normal focus:outline-line-focus disabled:border-line-normal disabled:text-text-assistive disabled:placeholder:text-text-assistive resize-none',
+          className,
+        )}
         style={
           errorText
             ? {
