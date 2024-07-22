17605923e8f1f85320e28081c817a39b2c60dcfa
jiseonjang <jsmeaningood@gmail.com>
2024-07-22 15:16:46 +0900

    🎨 css(custom): 삭제 버튼 폰트 스타일 추가

--- src/app/review/_components/delete-button/DeleteButton.tsx
+++ src/app/review/_components/delete-button/DeleteButton.tsx
@@ -4,7 +4,7 @@ export const DeleteButton = () => {
   return (
     <div className="flex items-center h-8 gap-1 self-end">
       <DeleteIcon size={20} />
-      <p>삭제</p>
+      <p className="font-body-13 text-text-strong">삭제</p>
     </div>
   );
 };
