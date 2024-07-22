757dd7c88c28a6a12766fb0ea4f936fea2b491b9
MeaninGood <jsmeaningood@gmail.com>
2024-07-22 10:28:45 +0900

    ✨ feat(custom): 지난주 하이라이트 / 로우라이트 개발

---
+++ src/app/review/step2/_components/LastReview/LastReview.tsx
@@ -0,0 +1,18 @@
+import ReviewItem from './ReviewItem';
+
+interface Props {
+  category: 'highLight' | 'lowLight';
+}
+
+export const LastReview = ({ category }: Props) => {
+  return (
+    <section className="flex flex-col gap-3 mb-8">
+      <p className="font-head-20 text-text-strong">
+        지난주 {category === 'highLight' ? '하이라이트' : '로우라이트'}
+      </p>
+      <div className="flex flex-col gap-3">
+        <ReviewItem />
+      </div>
+    </section>
+  );
+};
