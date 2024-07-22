3bc86eb03b212838ef50613375216786fb553847
jiseonjang <jsmeaningood@gmail.com>
2024-07-22 15:26:38 +0900

    ✨ feat(custom): tip 컴포넌트 개발

--- src/app/review/_components/current-review/CurrentReviewContainer.tsx
+++ src/app/review/_components/current-review/CurrentReviewContainer.tsx
@@ -2,7 +2,7 @@ import { CurrentReviewItem } from './CurrentReviewItem';
 
 export const CurrentReviewContainer = () => {
   return (
-    <div className="w-full p-5 bg-surface-foreground rounded-xl flex flex-col gap-6">
+    <div className="w-full p-5 bg-surface-foreground rounded-xl flex flex-col gap-6 mb-3">
       <CurrentReviewItem />
       <CurrentReviewItem />
       <CurrentReviewItem />
