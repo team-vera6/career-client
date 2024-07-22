b2ae5d28d24b4b78d7bac88a297fcbb0dea3c5ee
MeaninGood <jsmeaningood@gmail.com>
2024-07-22 10:29:05 +0900

    ✨ feat(custom): 이번주 하이라이트 / 로우라이트 개발

---
+++ src/app/review/step2/_components/CurrentReview/CurrentReview.tsx
@@ -0,0 +1,35 @@
+import { AddButton } from '@/app/review/_components/add-button/AddButton';
+import HighlightCircleIcon from '@/components/icons/HighlightCircleIcon';
+import LowlightCircleIcon from '@/components/icons/LowlightCircleIcon';
+import { ReviewContainer } from './ReviewContainer';
+
+interface Props {
+  category: 'highLight' | 'lowLight';
+}
+export const CurrentReview = ({ category }: Props) => {
+  return (
+    <section className="flex flex-col gap-3">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-0.5 items-center">
+          {ITEMS[category].icon}
+          <p className="font-head-20 text-text-strong">
+            {ITEMS[category].title}
+          </p>
+        </div>
+        <AddButton category={category} />
+      </div>
+      <ReviewContainer />
+    </section>
+  );
+};
+
+const ITEMS = {
+  highLight: {
+    icon: <HighlightCircleIcon size={24} />,
+    title: '하이라이트',
+  },
+  lowLight: {
+    icon: <LowlightCircleIcon size={24} />,
+    title: '로우라이트',
+  },
+};
---
+++ src/app/review/step2/_components/CurrentReview/ReviewContainer.tsx
@@ -0,0 +1,26 @@
+import Dropdown from '@/components/dropdown/Dropdown';
+import LinkIcon from '@/components/icons/LinkIcon';
+import Textarea from '@/components/inputs/textarea/Textarea';
+
+export const ReviewContainer = () => {
+  return (
+    <div className="w-full p-5 bg-surface-foreground rounded-xl">
+      <Textarea className="min-h-[6.5rem]" />
+      <div className="flex">
+        <LinkIcon size={36} />
+        <Dropdown id="hi" items={DROP_DUMMY} />
+      </div>
+    </div>
+  );
+};
+
+const DROP_DUMMY = [
+  {
+    name: 'test1',
+    value: 'test2',
+  },
+  {
+    name: 'test2',
+    value: 'test2',
+  },
+];
