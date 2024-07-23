afe3aef03bdd1b2c6ff6f13c740f60907475ff54
jiseonjang <jsmeaningood@gmail.com>
2024-07-23 16:34:25 +0900

    🪛 fix(custom): addButton css 수정

--- src/app/review/_components/add-button/AddButton.tsx
+++ src/app/review/_components/add-button/AddButton.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+
 import PlusIcon from '@/components/icons/PlusIcon';
+import colors from '@/styles/colors';
 import { cn } from '@/utils/tailwind';
 
 interface Props {
@@ -6,17 +11,34 @@ interface Props {
 }
 
 export const AddButton = ({ category }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const isTodo = category === 'currentTodo' || category === 'nextTodo';
+
   return (
     <button
       className={cn(
         'button-small',
-        category === 'currentTodo' || category === 'nextTodo'
+        isTodo
           ? 'button-text text-center'
-          : 'button-line',
+          : 'button-line hover:bg-surface-blank hover:opacity-100',
       )}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <PlusIcon size={20} />
-      <p>추가</p>
+      {isTodo ? (
+        <PlusIcon size={20} stroke={colors.text.normal} />
+      ) : (
+        <div className="h-5 w-5 flex items-center justify-center">
+          <PlusIcon
+            size={15}
+            stroke={isHovered ? colors.text.neutral : colors.text.strong}
+          />
+        </div>
+      )}
+      <p className={cn(isHovered ? 'text-text-neutral' : 'text-text-strong')}>
+        추가
+      </p>
     </button>
   );
 };
