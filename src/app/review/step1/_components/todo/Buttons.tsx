63f3ca5230a6009a68c6cd9ab5fc0e9fab17668c
jiseonjang <jsmeaningood@gmail.com>
2024-07-25 12:34:58 +0900

    ✨ feat(custom): 다음주로 이동/되돌리기 버튼 로직 개발

--- src/app/review/step1/_components/todo/Buttons.tsx
+++ src/app/review/step1/_components/todo/Buttons.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSetAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 
 import { currentTodoListAtom, nextTodoListAtom } from '@/app/review/stores';
 import { WeekType } from '@/app/review/types';
@@ -12,10 +12,32 @@ interface Props {
 }
 
 export const MoveNextButton = ({ week, id }: Props) => {
+  const [currentTodoList, setCurrentTodoList] = useAtom(currentTodoListAtom);
+  const [nextTodoList, setNextTodoList] = useAtom(nextTodoListAtom);
+
+  const onClickMove = () => {
+    if (week === 'current') {
+      const moveValues = currentTodoList.filter((el) => el.id === id)[0];
+      moveValues['week'] = 'next';
+      moveValues['isChecked'] = false;
+
+      setCurrentTodoList((prev) => prev.filter((el) => el.id !== id));
+      setNextTodoList((prev) => [...prev, moveValues]);
+    } else {
+      const moveValues = nextTodoList.filter((el) => el.id === id)[0];
+      moveValues['week'] = 'current';
+      moveValues['isChecked'] = false;
+
+      setNextTodoList((prev) => prev.filter((el) => el.id !== id));
+      setCurrentTodoList((prev) => [...prev, moveValues]);
+    }
+  };
+
   return (
     <button
       type="button"
       className="button-line button-small whitespace-nowrap"
+      onClick={() => onClickMove()}
     >
       {week === 'current' ? '다음주로 이동' : '되돌리기'}
     </button>
