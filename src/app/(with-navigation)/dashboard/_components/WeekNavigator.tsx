5c6e8e7d1ba21e3546f3a2abda7137e8ed555619
msdio <dev.msdio@gmail.com>
2024-07-26 16:53:10 +0900

    ♻️ rfac(custom): n월 m주차 네비게이터 컴포넌트 분리

---
+++ src/app/(with-navigation)/dashboard/_components/WeekNavigator.tsx
@@ -0,0 +1,27 @@
+import ChevronLeft20Icon from '@/components/icons/ChevronLeft20Icon';
+import ChevronRight20Icon from '@/components/icons/ChevronRight20Icon';
+
+interface Props {
+  month: number;
+  week: number;
+}
+
+const WeekNavigator = ({ month, week }: Props) => {
+  return (
+    <div className="flex items-center gap-2">
+      <p className="font-head-20 text-text-strong">
+        {month}월 {week}주차
+      </p>
+      <div className="flex gap-1.5">
+        <button className="bg-surface-foreground w-7 h-7 rounded-md flex items-center justify-center">
+          <ChevronLeft20Icon size={20} />
+        </button>
+        <button className="bg-surface-foreground w-7 h-7 rounded-md flex items-center justify-center">
+          <ChevronRight20Icon size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default WeekNavigator;
--- src/app/(with-navigation)/dashboard/page.tsx
+++ src/app/(with-navigation)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 
-import ChevronLeft20Icon from '@/components/icons/ChevronLeft20Icon';
-import ChevronRight20Icon from '@/components/icons/ChevronRight20Icon';
-
 import MemoList from './_components/MemoList';
 import Metrics from './_components/Metrics';
 import TodoList from './_components/TodoList';
+import WeekNavigator from './_components/WeekNavigator';
 
 export default function DashboardPage() {
   return (
@@ -14,17 +12,7 @@ export default function DashboardPage() {
 
       <div className="w-full">
         <section className="flex items-center justify-between mb-6">
-          <div className="flex items-center gap-2">
-            <p className="font-head-20 text-text-strong">6월 4주차</p>
-            <div className="flex gap-1.5">
-              <button className="bg-surface-foreground w-7 h-7 rounded-md flex items-center justify-center">
-                <ChevronLeft20Icon size={20} />
-              </button>
-              <button className="bg-surface-foreground w-7 h-7 rounded-md flex items-center justify-center">
-                <ChevronRight20Icon size={20} />
-              </button>
-            </div>
-          </div>
+          <WeekNavigator month={6} week={4} />
 
           <Link href="/review/step1">
             <button className="button-primary button-large">회고하기</button>
