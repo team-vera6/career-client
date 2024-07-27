3b769536c5781a2268155a4efccdcb9a4e65ffd7
msdio <dev.msdio@gmail.com>
2024-07-27 14:44:28 +0900

    ♻️ rfac(custom): 대시보드 메모 전역 상태로 관리

--- src/app/(with-navigation)/dashboard/_components/MemoList.tsx
+++ src/app/(with-navigation)/dashboard/_components/MemoList.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useAtom } from 'jotai';
 import Image from 'next/image';
 import { useState } from 'react';
 
+import { memoListAtom } from '@/app/review/stores';
 import EmptyMemoImage from '@/assets/images/memo-empty.png';
 import PlusIcon from '@/components/icons/PlusIcon';
 import Memo from '@/components/memo/Memo';
@@ -10,8 +12,7 @@ import TextEditorModal from '@/components/modal/text-editor';
 
 const MemoList = () => {
   const [openTextEditor, setOpenTextEditor] = useState(false);
-  const [memos, setMemos] =
-    useState<{ title: string; memo: string; date: string }[]>(dummy);
+  const [memos, setMemos] = useAtom(memoListAtom);
 
   return (
     <section className="shrink-0 min-w-[252px]">
@@ -51,9 +52,18 @@ const MemoList = () => {
           setOpenTextEditor(false);
         }}
         onSaveText={(text) => {
+          // setMemos((prev) => [
+          //   ...prev,
+          //   { title: '', memo: text, date: new Date().toLocaleDateString() },
+          // ]);
           setMemos((prev) => [
             ...prev,
-            { title: '', memo: text, date: new Date().toLocaleDateString() },
+            {
+              id: new Date().toString(),
+              title: '',
+              memo: text,
+              date: '7.22',
+            },
           ]);
         }}
       />
@@ -62,26 +72,3 @@ const MemoList = () => {
 };
 
 export default MemoList;
-
-const dummy: { title: string; memo: string; date: string }[] = [
-  {
-    title: '로열티 기획',
-    memo: '회원 등급, 포인트 시스템',
-    date: '7.12',
-  },
-  {
-    title: '제휴 브랜드 전용 새로운 테마, 템플릿 레퍼런스 찾기',
-    memo: '',
-    date: '7.12',
-  },
-  {
-    title: '주간회의',
-    memo: '- 신규입사자 안내\n- 진행건 요약정리\n- 마케팅 팀 신규 캠페인 관련 공유',
-    date: '7.12',
-  },
-  {
-    title: '',
-    memo: '개발팀에 서버 로그 재분석 요청 -> 롤백 계획 준비',
-    date: '7.12',
-  },
-];
