281a7bb27d528e55bc7ece94c8ac7e81aa15dbd6
MeaninGood <jsmeaningood@gmail.com>
2024-07-27 09:00:14 +0900

    🔧 add(custom): 더미데이터 데모데이용으로 변경

--- src/app/review/dummy.ts
+++ src/app/review/dummy.ts
@@ -7,32 +7,25 @@ export const CURRENT_TODO: TodoListItem[] = [
     id: 'current-1',
     week: 'current',
     isChecked: true,
-    todo: '프로젝트 진행 상황 팔로업',
+    todo: '신규 서비스 기획서 작성 마무리',
   },
   {
     id: 'current-2',
 
     week: 'current',
     isChecked: false,
-    todo: 'v1.2 기획분 요구사항 파악',
+    todo: '경영진 보고 준비',
   },
   {
     id: 'current-3',
 
     week: 'current',
     isChecked: false,
-    todo: '디자인시스템 개발 일정 산정 및 공수 파악',
+    todo: '동기들과 점심 식사',
   },
 ];
 
-export const NEXT_TODO: TodoListItem[] = [
-  {
-    id: 'next-1',
-    week: 'next',
-    isChecked: true,
-    todo: '디자인시스템 input 관련 개발',
-  },
-];
+export const NEXT_TODO: TodoListItem[] = [];
 
 export const HIGHLIGHT_REVIEW = {
   id: 'highLight-1',
@@ -68,27 +61,22 @@ export const MEMO_LIST: MemoItem[] = [
   {
     id: 'memo-1',
     isBookmark: false,
-    title: '로열티 기획',
-    memo: '회원 등급, 포인트 시스템',
-    date: '7.12',
+    title: '신규 서비스 개요',
+    memo: `목표: 서비스 사용자 만족도 향상 및 체류 시간 증가\n주요 기능: 사용자 맞춤 추천, 실시간 알림 시스템, 사용자 피드백 통합`,
+    date: '7.22',
   },
   {
     id: 'memo-2',
-    isBookmark: false,
-    title: '제휴 브랜드 전용 새로운 테마, 템플릿 레퍼런스 찾기',
-    date: '7.12',
+    isBookmark: true,
+    title: '리더님 피드백',
+    memo: `유관 부서에 지표 리포트할 때 항상 엑셀 파일 첨부하기\n엑셀 제목은 데이터 날짜 항상 표기하기!`,
+    date: '7.24',
   },
   {
     id: 'memo-3',
     isBookmark: false,
-    title: '주간회의',
-    memo: `• 신규입사자 안내\n• a진행건 요약정리\n• 마케팅 팀 신규 캠페인 관련 공유`,
-    date: '7.12',
-  },
-  {
-    id: 'memo-4',
-    isBookmark: false,
-    memo: '개발팀에 서버 로그 재분석 요청 -> 롤백 계획 준비',
-    date: '7.12',
+    title: '8/6 동기들이랑 점심 식사',
+    memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+    date: '7.25',
   },
 ];
