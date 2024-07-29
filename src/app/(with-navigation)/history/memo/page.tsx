0610042ec2e98d16e548337701856bbc7088738b
jiseonjang <jsmeaningood@gmail.com>
2024-07-29 16:19:12 +0900

    ✨ feat(custom): 회고/메모 페이지 구조 잡기, routing config 설정(history -\> history/review)

---
+++ src/app/(with-navigation)/history/memo/page.tsx
@@ -0,0 +1,9 @@
+import Category from '../_components/Category';
+
+export default function HistoryMemoPage() {
+  return (
+    <div className="w-full flex flex-col gap-7">
+      <Category />
+    </div>
+  );
+}
---
+++ src/app/(with-navigation)/history/review/page.tsx
@@ -0,0 +1,9 @@
+import Category from '../_components/Category';
+
+export default function HistoryReviewPage() {
+  return (
+    <div className="w-full flex flex-col gap-7">
+      <Category />
+    </div>
+  );
+}
