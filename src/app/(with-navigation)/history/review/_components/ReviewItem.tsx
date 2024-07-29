30cc47b71c709f5b768ff11fdf5fa6ce030e7632
jiseonjang <jsmeaningood@gmail.com>
2024-07-29 18:14:22 +0900

    ♻️ rfac(custom): reviewItem 불필요한 whitespace 제거

--- src/app/(with-navigation)/history/review/_components/ReviewItem.tsx
+++ src/app/(with-navigation)/history/review/_components/ReviewItem.tsx
@@ -12,7 +12,7 @@ const ReviewItem = ({ week, content, activeCount }: Props) => {
       <p className="w-12 mr-3 font-body-16 text-text-normal text-left">
         {week}주차
       </p>
-      <p className="flex-1 font-title-16 text-text-strong truncate whitespace-nowrap mr-4">
+      <p className="flex-1 font-title-16 text-text-strong truncate mr-4">
         {content}
       </p>
       <div className="px-3 py-2 rounded-[0.875rem] bg-surface-foreground">
