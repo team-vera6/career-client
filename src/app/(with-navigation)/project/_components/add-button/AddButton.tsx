470382fb55c5994b5bb3c8bdf4f85c14aaf6a47e
msdio <dev.msdio@gmail.com>
2024-07-30 13:52:52 +0900

    🪛 fix(custom): 프로젝트 생성 위치를 추가 버튼으로 이동

--- src/app/(with-navigation)/project/_components/add-button/AddButton.tsx
+++ src/app/(with-navigation)/project/_components/add-button/AddButton.tsx
@@ -1,14 +1,31 @@
+'use client';
+
+import { useState } from 'react';
+
+import CreateProjectSheet from '@/components/action-sheets/create-project/CreateProjectSheet';
 import PlusIcon from '@/components/icons/PlusIcon';
 
 const AddButton = () => {
+  const [showSheet, setShowSheet] = useState(false);
+
   return (
-    <div className="w-full flex items-center justify-between mb-[1.875rem]">
-      <p className="font-head-20 text-text-strong">프로젝트</p>
-      <button className="button-primary button-medium">
-        <PlusIcon size={20} stroke="#fff" />
-        <p className="font-body-14 text-text-invert">추가</p>
-      </button>
-    </div>
+    <>
+      <div className="w-full flex items-center justify-between mb-[1.875rem]">
+        <p className="font-head-20 text-text-strong">프로젝트</p>
+        <button
+          className="button-primary button-medium"
+          onClick={() => setShowSheet(true)}
+        >
+          <PlusIcon size={20} stroke="#fff" />
+          <p className="font-body-14 text-text-invert">추가</p>
+        </button>
+      </div>
+
+      <CreateProjectSheet
+        isOpen={showSheet}
+        closeSheet={() => setShowSheet(false)}
+      />
+    </>
   );
 };
 
--- src/app/(with-navigation)/project/_components/project-item/ProjectItem.tsx
+++ src/app/(with-navigation)/project/_components/project-item/ProjectItem.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useState } from 'react';
+
+import CreateProjectSheet from '@/components/action-sheets/create-project/CreateProjectSheet';
 import ProgressChip from '@/components/progress-chip/ProgressChip';
 
 interface Props {
@@ -6,19 +11,28 @@ interface Props {
 }
 
 const ProjectItem = ({ project, progress }: Props) => {
+  const [showSheet, setShowSheet] = useState(false);
+
   return (
-    <div
-      key={project}
-      className="w-full rounded-lg flex items-center justify-between gap-2.5 bg-surface-foreground px-5 py-3 hover:bg-surface-foregroundOn cursor-pointer"
-    >
-      <p
-        className="font-body-16 text-text-strong max-w-[53.375rem] text-ellipsis whitespace-nowrap overflow-hidden"
-        title={project}
+    <>
+      <div
+        key={project}
+        className="w-full rounded-lg flex items-center justify-between gap-2.5 bg-surface-foreground px-5 py-3 hover:bg-surface-foregroundOn cursor-pointer"
       >
-        {project}
-      </p>
-      <ProgressChip percentage={progress} />
-    </div>
+        <p
+          className="font-body-16 text-text-strong max-w-[53.375rem] text-ellipsis whitespace-nowrap overflow-hidden"
+          title={project}
+        >
+          {project}
+        </p>
+        <ProgressChip percentage={progress} />
+      </div>
+
+      <CreateProjectSheet
+        isOpen={showSheet}
+        closeSheet={() => setShowSheet(false)}
+      />
+    </>
   );
 };
 
