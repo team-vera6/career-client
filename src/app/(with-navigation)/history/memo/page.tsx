7cab1b061354e09ef749aa7e8f6aa53031853ec5
jiseonjang <jsmeaningood@gmail.com>
2024-08-01 19:37:45 +0900

    🎨 css(custom): 회고, 리뷰 컨테이너 불필요한 flex 관련 속성 제거

--- src/app/(with-navigation)/history/memo/page.tsx
+++ src/app/(with-navigation)/history/memo/page.tsx
@@ -3,7 +3,7 @@ import MemoHistory from './_components/MemoHistory/MemoHistory';
 
 export default function HistoryMemoPage() {
   return (
-    <div className="w-[60rem] flex flex-col gap-7">
+    <div className="w-[60rem]">
       <Category />
 
       <section className="flex flex-col gap-7">
--- src/app/(with-navigation)/history/review/page.tsx
+++ src/app/(with-navigation)/history/review/page.tsx
@@ -3,7 +3,7 @@ import HistoryReview from './_components/HistoryReview/HistoryReview';
 
 export default function HistoryReviewPage() {
   return (
-    <div className="w-[60rem] flex flex-col gap-7">
+    <div className="w-[60rem]">
       <Category />
 
       <section className="flex flex-col gap-8">
