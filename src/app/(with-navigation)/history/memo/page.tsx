1a1d684698cec7638b234c79a9812cd37b6f8f84
jiseonjang <jsmeaningood@gmail.com>
2024-08-01 19:31:49 +0900

    🪛 fix(custom): 회고, 메모 전체 너비 100%에서 60rem으로 수정

--- src/app/(with-navigation)/history/memo/page.tsx
+++ src/app/(with-navigation)/history/memo/page.tsx
@@ -1,9 +1,14 @@
 import Category from '../_components/Category';
+import MemoHistory from './_components/MemoHistory/MemoHistory';
 
 export default function HistoryMemoPage() {
   return (
-    <div className="w-full flex flex-col gap-7">
+    <div className="w-[60rem] flex flex-col gap-7">
       <Category />
+
+      <section className="flex flex-col gap-7">
+        <MemoHistory />
+      </section>
     </div>
   );
 }
