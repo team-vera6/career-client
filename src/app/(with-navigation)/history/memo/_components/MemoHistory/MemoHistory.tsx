57236a283322d14305d62abeba2fa7dfa58098e7
jiseonjang <jsmeaningood@gmail.com>
2024-08-01 19:19:46 +0900

    ✨ feat(custom): history/memo 메모 있을 때 화면 마크업

---
+++ src/app/(with-navigation)/history/memo/_components/MemoHistory/MemoHistory.tsx
@@ -0,0 +1,38 @@
+import Memo from '@/components/memo/Memo';
+
+import { dummy } from './dummy';
+
+const MemoHistory = () => {
+  return (
+    <>
+      {dummy.length ? (
+        <>
+          {dummy.map(({ month, week, items }) => (
+            <div
+              className="w-full flex flex-col gap-3"
+              key={`${month}-${week}`}
+            >
+              <p className="font-title-16 text-text-normal">
+                {month}월 {week}주
+              </p>
+
+              <div className="grid gap-[15px] grid-cols-4">
+                {items.map((item) => (
+                  <Memo
+                    key={item.id}
+                    {...item}
+                    className="w-[229px] h-[10rem]"
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <div>empty..</div>
+      )}
+    </>
+  );
+};
+
+export default MemoHistory;
---
+++ src/app/(with-navigation)/history/memo/_components/MemoHistory/dummy.ts
@@ -0,0 +1,98 @@
+export const dummy = [
+  {
+    month: 7,
+    week: 1,
+    items: [
+      {
+        id: 'memo-1',
+        isBookmark: false,
+        title: '신규 서비스 개요',
+        memo: `목표: 서비스 사용자 만족도 향상 및 체류 시간 증가\n주요 기능: 사용자 맞춤 추천, 실시간 알림 시스템, 사용자 피드백 통합`,
+        date: '7.22',
+      },
+      {
+        id: 'memo-2',
+        isBookmark: true,
+        title: '리더님 피드백',
+        memo: `유관 부서에 지표 리포트할 때 항상 엑셀 파일 첨부하기\n엑셀 제목은 데이터 날짜 항상 표기하기!`,
+        date: '7.24',
+      },
+      {
+        id: 'memo-3',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-4',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-5',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-6',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-7',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-8',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-9',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+      {
+        id: 'memo-10',
+        isBookmark: false,
+        title: '8/6 동기들이랑 점심 식사',
+        memo: '참여자: 봉주, 지선, 해람, 호, 유림',
+        date: '7.25',
+      },
+    ],
+  },
+  {
+    month: 6,
+    week: 4,
+    items: [
+      {
+        id: 'memo-11',
+        isBookmark: false,
+        title: '신규 서비스 개요',
+        memo: `목표: 서비스 사용자 만족도 향상 및 체류 시간 증가\n주요 기능: 사용자 맞춤 추천, 실시간 알림 시스템, 사용자 피드백 통합`,
+        date: '7.22',
+      },
+      {
+        id: 'memo-12',
+        isBookmark: true,
+        title: '리더님 피드백',
+        memo: `유관 부서에 지표 리포트할 때 항상 엑셀 파일 첨부하기\n엑셀 제목은 데이터 날짜 항상 표기하기!`,
+        date: '7.24',
+      },
+    ],
+  },
+];
