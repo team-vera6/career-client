683bf84f1dc8a6b436b3e721310b613d83c9ac99
jiseonjang <jsmeaningood@gmail.com>
2024-08-01 19:25:43 +0900

    🎨 css(custom): empty review p태그에 폰트 설정 적용

--- src/app/(with-navigation)/history/review/_components/HistoryReview/EmptyHistoryReview.tsx
+++ src/app/(with-navigation)/history/review/_components/HistoryReview/EmptyHistoryReview.tsx
@@ -5,13 +5,13 @@ import EmptyReviewImage from '@/assets/images/review-empty.png';
 const EmptyHistoryReview = () => {
   return (
     <div className="flex flex-col items-center gap-10 mt-[7rem]">
-      <p>아직 작성된 회고가 없어요</p>
+      <p className="font-body-16 text-text-normal">아직 작성된 회고가 없어요</p>
       <Image
         src={EmptyReviewImage}
         width={100}
         height={100}
         priority
-        alt="no-history-review"
+        alt="no-review-history"
       />
     </div>
   );
