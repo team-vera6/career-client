53c426c3c005e80dc4da571a153924ed993046ce
MeaninGood <jsmeaningood@gmail.com>
2024-08-04 15:40:51 +0900

    ♻️ rfac(custom): memo history return 구조 변경

--- src/app/(with-navigation)/history/memo/_components/MemoHistory/MemoHistory.tsx
+++ src/app/(with-navigation)/history/memo/_components/MemoHistory/MemoHistory.tsx
@@ -5,34 +5,23 @@ import { dummy } from './dummy';
 import EmptyMemoHistory from './EmptyMemoHistory';
 
 const MemoHistory = () => {
+  if (dummy.length < 1) return <EmptyMemoHistory />;
+
   return (
     <>
-      {dummy.length > 0 ? (
-        <>
-          {dummy.map(({ month, week, items }) => (
-            <div
-              className="w-full flex flex-col gap-3"
-              key={`${month}-${week}`}
-            >
-              <p className="font-title-16 text-text-normal">
-                {month}월 {week}주
-              </p>
+      {dummy.map(({ month, week, items }) => (
+        <div className="w-full flex flex-col gap-3" key={`${month}-${week}`}>
+          <p className="font-title-16 text-text-normal">
+            {month}월 {week}주
+          </p>
 
-              <div className="grid gap-[15px] grid-cols-4">
-                {items.map((item: MemoItem) => (
-                  <Memo
-                    key={item.id}
-                    {...item}
-                    className="w-[229px] h-[10rem]"
-                  />
-                ))}
-              </div>
-            </div>
-          ))}
-        </>
-      ) : (
-        <EmptyMemoHistory />
-      )}
+          <div className="grid gap-[15px] grid-cols-4">
+            {items.map((item: MemoItem) => (
+              <Memo key={item.id} {...item} className="w-[229px] h-[10rem]" />
+            ))}
+          </div>
+        </div>
+      ))}
     </>
   );
 };
