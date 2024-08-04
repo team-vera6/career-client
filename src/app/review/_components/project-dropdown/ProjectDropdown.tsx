974bcbd9fc4ff4908283ccec90d92a881bcd8791
MeaninGood <jsmeaningood@gmail.com>
2024-08-04 19:46:17 +0900

    ✨ feat(custom): 회고 project dropdown 새 프로젝트 추가 sheet 연결

--- src/app/review/_components/project-dropdown/ProjectDropdown.tsx
+++ src/app/review/_components/project-dropdown/ProjectDropdown.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useState } from 'react';
+
+import CreateProjectSheet from '@/components/action-sheets/create-project/CreateProjectSheet';
 import PlusIcon from '@/components/icons/PlusIcon';
 import colors from '@/styles/colors';
 
@@ -8,14 +11,23 @@ import Dropdown, {
 } from '../../../../components/dropdown/Dropdown';
 
 const ProjectDropdown = ({ ...rest }: DropdownProps) => {
+  const [showSheet, setShowSheet] = useState(false);
+
   return (
     <>
       <Dropdown {...rest}>
-        <li className="h-[2.75rem] py-3 px-5 hover:bg-surface-background flex items-center gap-1 text-text-primary">
+        <li
+          className="h-[2.75rem] py-3 px-5 hover:bg-surface-background flex items-center gap-1 text-text-primary"
+          onClick={() => setShowSheet(true)}
+        >
           <PlusIcon size={16} stroke={colors.text.primary} />
           새로운 프로젝트 추가
         </li>
       </Dropdown>
+      <CreateProjectSheet
+        isOpen={showSheet}
+        closeSheet={() => setShowSheet(false)}
+      />
     </>
   );
 };
