86c3a7cd5cb01d1ba0fffd0ef2426e76c72e4b58
MeaninGood <jsmeaningood@gmail.com>
2024-08-11 21:47:47 +0900

    ✨ feat(custom): api index 작성

---
+++ src/apis/index.ts
@@ -0,0 +1,68 @@
+import type { AxiosRequestConfig } from 'axios';
+import axios, { isAxiosError } from 'axios';
+
+const apiInstance = axios.create({
+  baseURL: 'https://api-pitstop.site',
+  responseType: 'json',
+});
+
+// 요청 인터셉터
+apiInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+// 응답 인터셉터
+apiInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (isAxiosError(error) && error.response) {
+      const errorResponse = {
+        status: error.response.status,
+        message: error.response.data?.message || 'An error occurred',
+        code: error.response.data?.code || 'UNKNOWN_ERROR',
+      };
+      return Promise.reject(errorResponse);
+    }
+    return Promise.reject(error);
+  },
+);
+
+export const typedGet = async <T>(url: string, config?: AxiosRequestConfig) => {
+  const response = await apiInstance.get<T>(url, config);
+  return response.data;
+};
+
+export const typedPost = async <T>(
+  url: string,
+  data: unknown,
+  config?: AxiosRequestConfig,
+) => {
+  const response = await apiInstance.post<T>(url, data, config);
+  return response.data;
+};
+
+export const typedPut = async <T>(
+  url: string,
+  data: unknown,
+  config?: AxiosRequestConfig,
+) => {
+  const response = await apiInstance.put<T>(url, data, config);
+  return response.data;
+};
+
+export const typedDelete = async <T>(
+  url: string,
+  config?: AxiosRequestConfig,
+) => {
+  const response = await apiInstance.delete<T>(url, config);
+  return response.data;
+};
