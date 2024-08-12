6235e35e2fb700ac788564afc22caa6a4a69a95d
MeaninGood <jsmeaningood@gmail.com>
2024-08-12 23:14:04 +0900

    ✨ feat(custom): 로그인 함수, 훅 개발

--- src/apis/auth/login.ts
+++ src/apis/auth/login.ts
@@ -1,7 +1,24 @@
 import { typedPost } from '..';
 
-type LoginResponseType = Awaited<typeof login>;
+export const LOGIN_ERROR_MESSAGE: Record<string, string> = {
+  incorrect_password: '비밀번호가 일치하지 않습니다.',
+  invalid_request: '이메일을 확인해주세요.',
+};
 
-export const login = (email: string, password: string) => {
-  return typedPost<LoginResponseType>('/auth/sign-in', { email, password });
+export const login = async (email: string, password: string) => {
+  return typedPost<{ accessToken: string }>('/auth/sign-in', {
+    email,
+    password,
+  })
+    .then((res) => {
+      const accessToken = res.accessToken;
+      localStorage.setItem('accessToken', accessToken);
+      return { accessToken: accessToken };
+    })
+    .catch((err) => {
+      if ((err.code as string) && LOGIN_ERROR_MESSAGE[err.code]) {
+        return { errorMessage: LOGIN_ERROR_MESSAGE[err.code] };
+      }
+      return err;
+    });
 };
---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,27 @@
+import { useSetAtom } from 'jotai';
+
+import { login } from '@/apis/auth/login';
+import { accessTokenAtom } from '@/stores/user/accessTokenAtom';
+
+import useToast from './useToast';
+
+export const useUser = () => {
+  const SetAccessToken = useSetAtom(accessTokenAtom);
+  const { addToast } = useToast();
+
+  // login
+  const userLogin = async (email: string, password: string) => {
+    const result = await login(email, password);
+
+    if (result.accessToken) {
+      SetAccessToken(result.accessToken);
+    } else if (result.errorMessage) {
+      addToast({
+        message: result.errorMessage,
+        iconType: 'error',
+      });
+    }
+  };
+
+  return userLogin;
+};
