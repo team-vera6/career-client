5e38323c9b11f966f137b55f593e022e7ac6bf68
MeaninGood <jsmeaningood@gmail.com>
2024-08-18 00:13:45 +0900

    ✨ feat(custom): returnError 함수 작성, emailCheck post 작성

--- src/apis/auth/post.ts
+++ src/apis/auth/post.ts
@@ -2,8 +2,19 @@ import { ErrorResponseType } from '../../types/apis';
 import { typedPost } from '..';
 
 export const LOGIN_ERROR_MESSAGE: Record<string, string> = {
-  incorrect_password: '비밀번호가 일치하지 않습니다.',
-  resource_not_found: '이메일을 확인해주세요.',
+  incorrect_password: '이메일 또는 비밀번호를 다시 확인해 주세요.',
+  resource_not_found: '올바른 이메일 주소를 입력해 주세요.',
+  already_exist_email: '이미 등록된 이메일입니다.',
+};
+
+// 공통 error 리턴
+const returnError = (err: unknown) => {
+  const error = err as ErrorResponseType;
+
+  if ((error.code as string) && LOGIN_ERROR_MESSAGE[error.code]) {
+    return { errorMessage: LOGIN_ERROR_MESSAGE[error.code] };
+  }
+  return { errorMessage: 'Unknown error' };
 };
 
 export const login = async (email: string, password: string) => {
@@ -18,11 +29,21 @@ export const login = async (email: string, password: string) => {
 
     return { accessToken: accessToken };
   } catch (err) {
-    const error = err as ErrorResponseType;
+    return returnError(err);
+  }
+};
 
-    if ((error.code as string) && LOGIN_ERROR_MESSAGE[error.code]) {
-      return { errorMessage: LOGIN_ERROR_MESSAGE[error.code] };
-    }
-    return { errorMessage: 'Unknown error' };
+interface EmailCheckResponse {
+  id: string;
+  email: string;
+}
+
+export const emailCheck = async (email: string) => {
+  try {
+    return typedPost<EmailCheckResponse>('/auth/email', {
+      email,
+    });
+  } catch (err) {
+    return returnError(err);
   }
 };
