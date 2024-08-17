32bf9f51f614089fa19e04030c68ae8374dc33d0
MeaninGood <jsmeaningood@gmail.com>
2024-08-17 17:45:41 +0900

    🪛 fix(custom): error res type 추가, then catch -\> try catch로 변경

--- src/apis/auth/login.ts
+++ src/apis/auth/login.ts
@@ -1,24 +1,28 @@
 import { typedPost } from '..';
+import { ErrorResponseType } from './../../types/apis';
 
 export const LOGIN_ERROR_MESSAGE: Record<string, string> = {
   incorrect_password: '비밀번호가 일치하지 않습니다.',
-  invalid_request: '이메일을 확인해주세요.',
+  resource_not_found: '이메일을 확인해주세요.',
 };
 
 export const login = async (email: string, password: string) => {
-  return typedPost<{ accessToken: string }>('/auth/sign-in', {
-    email,
-    password,
-  })
-    .then((res) => {
-      const accessToken = res.accessToken;
-      localStorage.setItem('accessToken', accessToken);
-      return { accessToken: accessToken };
-    })
-    .catch((err) => {
-      if ((err.code as string) && LOGIN_ERROR_MESSAGE[err.code]) {
-        return { errorMessage: LOGIN_ERROR_MESSAGE[err.code] };
-      }
-      return err;
+  try {
+    const res = await typedPost<{ accessToken: string }>('/auth/sign-in', {
+      email,
+      password,
     });
+
+    const accessToken = res.accessToken;
+    localStorage.setItem('accessToken', accessToken);
+
+    return { accessToken: accessToken };
+  } catch (err) {
+    const error = err as ErrorResponseType;
+
+    if ((error.code as string) && LOGIN_ERROR_MESSAGE[error.code]) {
+      return { errorMessage: LOGIN_ERROR_MESSAGE[error.code] };
+    }
+    return err;
+  }
 };
---
+++ src/types/apis.ts
@@ -0,0 +1,5 @@
+export interface ErrorResponseType {
+  status: number;
+  message: string;
+  code: string;
+}
