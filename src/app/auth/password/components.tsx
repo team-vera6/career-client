caffae1fd536eed8d6d3174f07e03377611043cc
MeaninGood <jsmeaningood@gmail.com>
2024-08-18 23:10:20 +0900

    ✨ feat(custom): 비밀번호 유효성 검사, 일치 여부 확인 로직 개발

--- src/app/auth/password/components.tsx
+++ src/app/auth/password/components.tsx
@@ -4,21 +4,82 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 import PasswordInput from '@/components/inputs/password/PasswordInput';
+import { FormEvent, useMemo, useState } from 'react';
+import useToast from '@/hooks/useToast';
 
 const PasswordComponents = () => {
   const router = useRouter();
+  const { addToast } = useToast();
+
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showErrorText, setShowErrorText] = useState(false); // FIXME: 디자인 선반영 필요
+
+  const isValidate = (pwd: string) => {
+    const MIN_LENGTH = 8;
+
+    const hasUpperCase = /[A-Z]/.test(pwd);
+    const hasLowerCase = /[a-z]/.test(pwd);
+    const hasNumber = /[0-9]/.test(pwd);
+
+    if (
+      pwd.length < MIN_LENGTH ||
+      !(hasUpperCase && hasLowerCase && hasNumber)
+    ) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const handlePasswordChange = (pwd: string) => {
+    setPassword(pwd);
+    setShowErrorText(() => isValidate(pwd));
+  };
+
+  const handleConfirmPassword = (pwd: string) => {
+    setConfirmPassword(pwd);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      addToast({
+        message: '비밀번호가 일치하지 않습니다.',
+        iconType: 'error',
+      });
+    }
+    // FIXME: api 전송 로직 추가
+  };
+
+  const isButtonEnabled = useMemo(() => {
+    const isValidPassword = isValidate(password);
+    return isValidPassword && confirmPassword.length > 0;
+  }, [password, confirmPassword]);
 
   return (
-    <div className="flex flex-col items-center w-[22.25rem]">
+    <form
+      className="flex flex-col items-center w-[22.25rem]"
+      onSubmit={handleSubmit}
+    >
       <div className="flex flex-col w-full gap-3 mb-6">
-        <PasswordInput placeholder="비밀번호 입력" />
-        <PasswordInput placeholder="비밀번호 확인" />
+        <PasswordInput
+          placeholder="비밀번호 입력"
+          onChange={(e) => handlePasswordChange(e.currentTarget.value)}
+          value={password}
+        />
+        <PasswordInput
+          placeholder="비밀번호 확인"
+          onChange={(e) => handleConfirmPassword(e.currentTarget.value)}
+          value={confirmPassword}
+        />
       </div>
 
       <button
-        type="button"
+        type="submit"
         className="button-primary h-12 rounded-xl w-full mb-4"
-        onClick={() => router.push('/')}
+        disabled={!isButtonEnabled}
       >
         완료
       </button>
@@ -33,7 +94,7 @@ const PasswordComponents = () => {
         </Link>
         의 내용을 확인한 것으로 간주됩니다.
       </p>
-    </div>
+    </form>
   );
 };
 
