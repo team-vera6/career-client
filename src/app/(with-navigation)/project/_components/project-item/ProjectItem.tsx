84a96becbde5d7322381c65539e4715c1edfbe5b
msdio <dev.msdio@gmail.com>
2024-08-19 02:17:08 +0900

    ✨ feat(custom): 프로젝트 리스트 api 적용

--- src/app/(with-navigation)/project/_components/project-item/ProjectItem.tsx
+++ src/app/(with-navigation)/project/_components/project-item/ProjectItem.tsx
@@ -6,25 +6,26 @@ import ProjectDetailSheet from '@/components/action-sheets/project-detail/Projec
 import ProgressChip from '@/components/progress-chip/ProgressChip';
 
 interface Props {
-  project: string;
+  id: number;
+  title: string;
   progress: number;
 }
 
-const ProjectItem = ({ project, progress }: Props) => {
+const ProjectItem = ({ id, title, progress }: Props) => {
   const [showSheet, setShowSheet] = useState(false);
 
   return (
     <>
       <div
-        key={project}
+        key={id}
         className="w-full rounded-lg flex items-center justify-between gap-2.5 bg-surface-foreground px-5 py-3 hover:bg-surface-foregroundOn cursor-pointer"
         onClick={() => setShowSheet(true)}
       >
         <p
           className="font-body-16 text-text-strong max-w-[53.375rem] text-ellipsis whitespace-nowrap overflow-hidden"
-          title={project}
+          title={title}
         >
-          {project}
+          {title}
         </p>
         <ProgressChip percentage={progress} />
       </div>
