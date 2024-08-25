10180489c44256141343a7e3eb30785dda3071dc
msdio <dev.msdio@gmail.com>
2024-08-25 22:23:14 +0900

    ✨ feat(custom): 리포트 페이지 리스트 get api 작성

---
+++ src/apis/reports/get.ts
@@ -0,0 +1,20 @@
+import { CurrentWeek } from '@/types/currentWeek';
+
+import { typedGet } from '..';
+
+export const getReviewList = async () => {
+  const response = await typedGet<ReviewList>('/review');
+  return response;
+};
+
+interface Review {
+  weekNumber: CurrentWeek;
+  id: number;
+  like: number;
+  highlightSummary: string;
+}
+
+interface ReviewList {
+  reviews: Review[];
+  nextCursor: number;
+}
