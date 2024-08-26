fd4060965721f7efccfc6d404f592cea36aa4a52
jiseonjang <jsmeaningood@gmail.com>
2024-08-26 19:27:02 +0900

    🪛 fix(custom): nicknameFromEmail에서 이메일 형태가 아닌 경우 return 조건 수정

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,7 +1,6 @@
 export const nicknameFromEmail = (email: string) => {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!emailPattern) return '';
+  if (!emailPattern.test(email)) return '';
 
   const [nickname] = email.split('@');
   return nickname;
