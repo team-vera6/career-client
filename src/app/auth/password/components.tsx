55c6df8ca9640dca71efdae3b129e36d3e850253
jiseonjang <jsmeaningood@gmail.com>
2024-08-26 18:57:19 +0900

    🪛 fix(custom): password 페이지 유효성 함수명 변경, early return 추가, 개행 추가 및 submit return 추가

--- src/app/auth/password/components.tsx
+++ src/app/auth/password/components.tsx
@@ -30,22 +30,21 @@ const PasswordComponents = () => {
     }
   }, [email, router]);
 
-  const isValidate = (pwd: string) => {
+  const isPasswordValid = (pwd: string) => {
     const MIN_LENGTH = 8;
 
     const hasLetter = /[a-z]/i.test(pwd);
     const hasNumber = /[0-9]/.test(pwd);
 
-    if (pwd.length < MIN_LENGTH || !(hasLetter && hasNumber)) {
-      return false;
-    }
+    if (pwd.length < MIN_LENGTH) return false;
+    if (!(hasLetter && hasNumber)) return false;
 
     return true;
   };
 
   const handlePasswordChange = (pwd: string) => {
     setPassword(pwd);
-    setShowErrorText(() => !isValidate(pwd));
+    setShowErrorText(() => !isPasswordValid(pwd));
   };
 
   const handleConfirmPassword = (pwd: string) => {
@@ -60,26 +59,27 @@ const PasswordComponents = () => {
         message: '비밀번호가 일치하지 않습니다.',
         iconType: 'error',
       });
-    } else {
-      const nickname = nicknameFromEmail(email);
+      return;
+    }
 
-      const req = {
-        emailId: id,
-        password,
-        nickname,
-      };
+    const nickname = nicknameFromEmail(email);
 
-      const res = await userSignUp(req);
+    const req = {
+      emailId: id,
+      password,
+      nickname,
+    };
 
-      if (res === 'success') {
-        setIsSignUp(true); // 최초 회원가입 시 띄워줄 모달 상태
-        router.push('/');
-      }
+    const res = await userSignUp(req);
+
+    if (res === 'success') {
+      setIsSignUp(true); // 최초 회원가입 시 띄워줄 모달 상태
+      router.push('/');
     }
   };
 
   const isButtonEnabled = useMemo(() => {
-    const isValidPassword = isValidate(password);
+    const isValidPassword = isPasswordValid(password);
     return isValidPassword && confirmPassword.length > 0;
   }, [password, confirmPassword]);
 
@@ -94,11 +94,13 @@ const PasswordComponents = () => {
           onChange={(e) => handlePasswordChange(e.currentTarget.value)}
           value={password}
         />
+
         {showErrorText && (
           <p className="font-body-12 text-state-negative">
             비밀번호는 8자 이상, 영문, 숫자를 포함해 주세요.
           </p>
         )}
+
         <PasswordInput
           placeholder="비밀번호 확인"
           onChange={(e) => handleConfirmPassword(e.currentTarget.value)}
@@ -113,6 +115,7 @@ const PasswordComponents = () => {
       >
         완료
       </button>
+
       <p className="font-body-13 text-text-strong">
         서비스 가입 시{' '}
         <Link href="/" className="underline">
