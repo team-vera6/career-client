da9c303430441f38baafa5a1cd21a39a7a76578f
msdio <dev.msdio@gmail.com>
2024-08-27 00:12:18 +0900

    ✨ feat(custom): 메모 그룹핑한 MemoWeekGroup 컴포넌트 제작

---
+++ src/app/(with-navigation)/history/memo/_components/MemoHistory/MemoWeekGroup.tsx
@@ -0,0 +1,36 @@
+import { Memo as MemoType } from '@/apis/memo/get';
+import Memo from '@/components/memo/Memo';
+import { CurrentWeek } from '@/types/currentWeek';
+
+interface Props {
+  currentWeek: CurrentWeek;
+  memos: MemoType[];
+}
+
+const MemoWeekGroup = ({ currentWeek, memos }: Props) => {
+  return (
+    <div
+      className="w-full flex flex-col gap-3"
+      key={`${currentWeek.year}-${currentWeek.month}-${currentWeek.week}`}
+    >
+      <p className="font-title-16 text-text-normal">
+        {currentWeek.month}월 {currentWeek.week}주
+      </p>
+
+      <div className="grid gap-[15px] grid-cols-4">
+        {memos.map((item) => (
+          <Memo
+            key={item.id}
+            id={String(item.id)}
+            title={item.content}
+            date="7.22"
+            isBookmark={item.isMarked}
+            className="w-[229px] h-[10rem]"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MemoWeekGroup;
