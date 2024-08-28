fdb275b6c1c9c1e17016e768a7495e06c4ded76a
MeaninGood <jsmeaningood@gmail.com>
2024-08-28 21:18:17 +0900

    ✨ feat(custom): 회고 주차 정보 반영

--- src/app/review/_components/week-info/WeekInfo.tsx
+++ src/app/review/_components/week-info/WeekInfo.tsx
@@ -3,12 +3,16 @@ import { getCurrentWeek } from '@/utils/date';
 import { ActivePage } from './ActivePage';
 
 export const WeekInfo = () => {
-  const { month, week } = getCurrentWeek();
+  const { month, week, startMonth, startDay, endMonth, endDay } =
+    getCurrentWeek();
 
   return (
     <section className="flex justify-between items-center mb-6">
       <p className="font-head-24 text-text-strong">
-        {month}월 {week}주차<sup className="font-body-14 ml-2">7/22 ~ 7/26</sup>
+        {month}월 {week}주차
+        <sup className="font-body-14 ml-2">
+          {startMonth}/{startDay} ~ {endMonth}/{endDay}
+        </sup>
       </p>
       <ActivePage />
     </section>
