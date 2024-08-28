6c3a41bc0c46fbd7f4c61968ebd924816bc38925
msdio <dev.msdio@gmail.com>
2024-08-28 10:59:02 +0900

    ✨ feat(custom): report 페이지 api 작성

--- src/apis/reports/get.ts
+++ src/apis/reports/get.ts
@@ -1,11 +1,7 @@
 import { CurrentWeek } from '@/types/currentWeek';
+import { Highlight } from '@/types/highlight';
 
 import { typedGet } from '..';
-
-export const getReviewList = async () => {
-  const response = await typedGet<ReviewList>('/review');
-  return response;
-};
 
 export interface Review {
   weekNumber: CurrentWeek;
@@ -18,3 +14,30 @@ interface ReviewList {
   reviews: Review[];
   nextCursor: number;
 }
+
+export const getReviewList = async () => {
+  const response = await typedGet<ReviewList>('/review');
+  return response;
+};
+
+type HighlightResponse = {
+  highlights: Omit<Highlight, 'currentWeek'>[];
+};
+
+export const getHighlights = async ({ year, month, week }: CurrentWeek) => {
+  const response = await typedGet<HighlightResponse>('/highlight', {
+    params: { year, month, week },
+  });
+  return response;
+};
+
+type LowResponse = {
+  lowlights: Omit<Highlight, 'currentWeek'>[];
+};
+
+export const getLowlights = async ({ year, month, week }: CurrentWeek) => {
+  const response = await typedGet<LowResponse>('/lowlights', {
+    params: { year, month, week },
+  });
+  return response;
+};
