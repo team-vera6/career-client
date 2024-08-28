3dd66952c60c64fe80e963a75ce97644c95c6f33
msdio <dev.msdio@gmail.com>
2024-08-28 22:44:47 +0900

    🪛 fix(custom): project -\> projects 이름 변경

--- src/apis/projects/post.ts
+++ src/apis/projects/post.ts
@@ -3,6 +3,6 @@ import { Project } from '@/types/project';
 import { typedPost } from '..';
 
 export const addProject = async (body: Project) => {
-  const response = await typedPost('/project', body);
+  const response = await typedPost('/projects', body);
   return response;
 };
