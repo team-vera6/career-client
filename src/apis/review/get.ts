031fd519d174e7b2abaf2ffbd556873c17d468d4
MeaninGood <jsmeaningood@gmail.com>
2024-09-02 14:16:13 +0900

    ✨ feat(custom): 하이라이트,로우라이트 조회 api 개발

--- src/apis/review/get.ts
+++ src/apis/review/get.ts
@@ -18,3 +18,27 @@ export const getTodos = async ({ year, month, week }: CurrentWeek) => {
   });
   return response;
 };
+
+export interface Review {
+  id: number;
+  content: string;
+  project: {
+    id: number;
+    content: string;
+    progressRate: number;
+  };
+}
+
+export const getHighlightList = async ({ year, month, week }: CurrentWeek) => {
+  const response = await typedGet<{ highlights: Review[] }>('/highlights', {
+    params: { year, month, week },
+  });
+  return response;
+};
+
+export const getLowlightList = async ({ year, month, week }: CurrentWeek) => {
+  const response = await typedGet<{ lowlights: Review[] }>('/lowlights', {
+    params: { year, month, week },
+  });
+  return response;
+};
