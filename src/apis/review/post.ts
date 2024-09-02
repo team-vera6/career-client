db17604c5dca22cb2815a6f6b32d251761967331
MeaninGood <jsmeaningood@gmail.com>
2024-09-02 14:23:08 +0900

    ✨ feat(custom): 하이라이트/로우라이트 추가 api 개발

--- src/apis/review/post.ts
+++ src/apis/review/post.ts
@@ -15,3 +15,28 @@ export const addScore = async (body: Score) => {
   const response = await typedPost<ScoreResponse>('/reviews', body);
   return response;
 };
+
+export interface ReviewProject {
+  content: string;
+  projectId: number;
+}
+
+export interface HighlightsRequest {
+  reviewId: number;
+  highlights: ReviewProject[];
+}
+
+export const addHighlights = async (body: HighlightsRequest) => {
+  const response = await typedPost<{ id: number[] }>('/highlights', body);
+  return response;
+};
+
+export interface LowlightsRequest {
+  reviewId: number;
+  lowlights: ReviewProject[];
+}
+
+export const addLowlights = async (body: LowlightsRequest) => {
+  const response = await typedPost<{ id: number[] }>('/lowlights', body);
+  return response;
+};
