aab21b052e42d85499c9e1d7078680b4a4726896
MeaninGood <jsmeaningood@gmail.com>
2024-09-02 22:38:21 +0900

    ✨ feat(custom): todolist에 다음주 할일 api 추가

--- src/app/review/step1/_components/todo/TodoList.tsx
+++ src/app/review/step1/_components/todo/TodoList.tsx
@@ -1,40 +1,58 @@
 'use client';
 
-import { useAtom, useAtomValue } from 'jotai';
+import { useAtom } from 'jotai';
 import { useEffect, useMemo } from 'react';
 
 import { getTodos } from '@/apis/review/get';
 import { currentTodoListAtom, nextTodoListAtom } from '@/app/review/stores';
 import { TodoListItem } from '@/app/review/types';
-import { getCurrentWeek } from '@/utils/date';
+import { getCurrentWeek, getNextWeek } from '@/utils/date';
 
 import { ListItem } from './ListItem';
 
 export const TodoList = ({ week }: Pick<TodoListItem, 'week'>) => {
   const [currentTodoList, setCurrentTodoList] = useAtom(currentTodoListAtom);
-  const nextTodoList = useAtomValue(nextTodoListAtom);
+  const [nextTodoList, setNextTodoList] = useAtom(nextTodoListAtom);
   const { year, month, week: weekNumber } = getCurrentWeek();
+  const { nextYear, nextMonth, nextWeek } = getNextWeek();
 
   useEffect(() => {
-    if (week !== 'current') return;
+    if (week === 'current') {
+      (async () => {
+        const response = await getTodos({
+          year,
+          week: weekNumber,
+          month,
+        });
 
-    (async () => {
-      const response = await getTodos({
-        year,
-        week: weekNumber,
-        month,
-      });
+        setCurrentTodoList(
+          response.todos.map((el) => ({
+            week: 'current',
+            isChecked: el.status === 'DONE',
+            todo: el.content,
+            id: String(el.id),
+          })),
+        );
+      })();
+    } else {
+      (async () => {
+        const response = await getTodos({
+          year: nextYear,
+          week: nextMonth,
+          month: nextWeek,
+        });
 
-      setCurrentTodoList(
-        response.todos.map((el) => ({
-          week: 'current',
-          isChecked: el.status === 'DONE',
-          todo: el.content,
-          id: String(el.id),
-        })),
-      );
-    })();
-  }, [month, setCurrentTodoList, week, weekNumber, year]);
+        setNextTodoList(
+          response.todos.map((el) => ({
+            week: 'next',
+            isChecked: el.status === 'DONE',
+            todo: el.content,
+            id: String(el.id),
+          })),
+        );
+      })();
+    }
+  }, [month, setCurrentTodoList, setNextTodoList, week, weekNumber, year]);
 
   const todoList = useMemo(() => {
     return week === 'current' ? currentTodoList : nextTodoList;
