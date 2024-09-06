5f4cd47393e26941114d7646f040372c30b4d3b3
MeaninGood <jsmeaningood@gmail.com>
2024-09-07 00:04:00 +0900

    🪛 fix(custom): 다음 버튼 클릭 시 api 전송하던 부분 삭제 (등록 클릭 시 일괄 전송 예정)

--- src/app/review/step1/_components/paging-button/PagingButton.tsx
+++ src/app/review/step1/_components/paging-button/PagingButton.tsx
@@ -1,55 +1,21 @@
 'use client';
 
-import { useAtomValue, useSetAtom } from 'jotai';
+import { useAtomValue } from 'jotai';
 
-import { addScore } from '@/apis/review/post';
-import {
-  pageButtonStatesAtom,
-  reviewIdAtom,
-  scoreAtom,
-} from '@/app/review/stores';
+import { pageButtonStatesAtom } from '@/app/review/stores';
 import { usePagingButton } from '@/app/review/utils';
-import useToast from '@/hooks/useToast';
-import { getCurrentWeek } from '@/utils/date';
-
-const { year, month, week } = getCurrentWeek();
 
 export const PagingButton = () => {
   const { onClickPagingButton } = usePagingButton();
-  const { addToast } = useToast();
 
   const pageButtonStates = useAtomValue(pageButtonStatesAtom);
-  const selectedScore = useAtomValue(scoreAtom);
-  const setReviewId = useSetAtom(reviewIdAtom);
-
-  const onClickButton = async () => {
-    const body = {
-      weekNumber: {
-        year,
-        month,
-        week,
-      },
-      like: selectedScore,
-    };
-
-    try {
-      const response = await addScore(body);
-      setReviewId(response.id);
-      onClickPagingButton({ path: 'step2', activePage: 2 });
-    } catch (err) {
-      addToast({
-        message: '다시 시도해주세요.',
-        iconType: 'error',
-      });
-    }
-  };
 
   return (
     <div className="flex justify-end">
       <button
         type="button"
         className="button-primary button-large"
-        onClick={onClickButton}
+        onClick={() => onClickPagingButton({ path: 'step2', activePage: 2 })}
         disabled={!pageButtonStates['step1']}
       >
         다음
