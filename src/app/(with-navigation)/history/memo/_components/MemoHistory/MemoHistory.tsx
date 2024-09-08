26b1a780110fd765827ee6a30b5140910ab66120
msdio <dev.msdio@gmail.com>
2024-09-09 01:06:21 +0900

    🪛 fix(custom): 메모 그룹핑, 메모 렌더링 로직 수정

--- src/app/(with-navigation)/history/memo/_components/MemoHistory/MemoHistory.tsx
+++ src/app/(with-navigation)/history/memo/_components/MemoHistory/MemoHistory.tsx
@@ -3,30 +3,76 @@
 import { useEffect, useState } from 'react';
 
 import { AllMemosResponse, getAllMemos } from '@/apis/memo/get';
+import { Memo } from '@/types/memo';
+import { sortByWeek } from '@/types/sort';
 
 import EmptyMemoHistory from './EmptyMemoHistory';
+import MemoWeekGroup from './MemoWeekGroup';
 
 const MemoHistory = () => {
-  const [memos, setMemos] = useState<AllMemosResponse>();
+  const [memos, setMemos] = useState<
+    {
+      weekNumber: { year: number; month: number; week: number };
+      memos: Memo[];
+    }[]
+  >([]);
+
+  const groupMemosByWeek = (receivedMemos: AllMemosResponse) => {
+    const groupedMemos = receivedMemos.contents.reduce(
+      (acc, cur) => {
+        const { year, month, week } = cur.weekNumber;
+
+        const item = {
+          id: cur.id,
+          content: cur.content,
+          isMarked: cur.isMarked,
+          updatedAt: cur.updatedAt,
+        };
+
+        const key = `${year}-${month}-${week}`;
+        if (acc[key]) {
+          acc[key].memos.push(item);
+        } else {
+          acc[key] = {
+            weekNumber: cur.weekNumber,
+            memos: [item],
+          };
+        }
+
+        return acc;
+      },
+      {} as Record<
+        string,
+        {
+          weekNumber: { year: number; month: number; week: number };
+          memos: Memo[];
+        }
+      >,
+    );
+
+    return Object.values(groupedMemos).sort(sortByWeek);
+  };
 
   useEffect(() => {
     (async () => {
       const response = await getAllMemos({});
-      setMemos(response);
+
+      const sorted = groupMemosByWeek(response);
+      setMemos(sorted);
     })();
   }, []);
 
-  if (!memos || memos.contents.length === 0) return <EmptyMemoHistory />;
+  if (memos.length === 0) return <EmptyMemoHistory />;
 
   return (
     <>
-      {/* {memos.contents.map(({ weekNumber, memos }) => (
+      {memos.map(({ weekNumber, memos }) => (
         <MemoWeekGroup
           key={`${weekNumber.year}-${weekNumber.month}-${weekNumber.week}`}
           currentWeek={weekNumber}
           memos={memos}
         />
-      ))} */}
+      ))}
       <p>memo</p>
     </>
   );
