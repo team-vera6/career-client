6177ff833162e1562b400382917afb7781d550e4
MeaninGood <jsmeaningood@gmail.com>
2024-09-09 01:33:35 +0900

    ✨ feat(custom): 지난주 하이라이트/로우라이트 가져오는 로직 추가

--- src/app/review/_components/last-week-review/LastWeekReviewItem.tsx
+++ src/app/review/_components/last-week-review/LastWeekReviewItem.tsx
@@ -1,27 +1,25 @@
 import LinkIcon from '@/components/icons/LinkIcon';
 import ProgressChip from '@/components/progress-chip/ProgressChip';
 
-import { LastWeekReviewListItem } from '../../types';
+import { ReviewListItem } from '../../types';
 
-export const LastWeekReviewItem = ({
-  text,
-  project,
-  progressCount,
-}: LastWeekReviewListItem) => {
+export const LastWeekReviewItem = ({ content, project }: ReviewListItem) => {
   return (
     <div className="w-full">
       <div
         className="p-3 border border-line-assistive rounded-lg bg-surface-foreground
      font-body-14 text-text-strong"
       >
-        {text}
+        {content}
       </div>
 
       <div className="flex">
         <LinkIcon size={36} />
         <div className="w-[calc(100%-2.2rem)] flex items-center justify-between gap-2 py-1.5 pl-3 pr-2 mt-2 border border-line-normal rounded-lg bg-surface-foregroundOn">
-          <p className="truncate font-body-14 text-text-strong">{project}</p>
-          <ProgressChip percentage={progressCount} />
+          <p className="truncate font-body-14 text-text-strong">
+            {project.content}
+          </p>
+          <ProgressChip percentage={project.progressRate} />
         </div>
       </div>
     </div>
