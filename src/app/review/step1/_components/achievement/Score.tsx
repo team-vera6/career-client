de5e84ac5c1694c3c2c7c5fcc99111943cbe8c16
MeaninGood <jsmeaningood@gmail.com>
2024-09-12 15:32:12 +0900

    🪛 fix(custom): 만족도 get 호출 위치 변경

--- src/app/review/step1/_components/achievement/Score.tsx
+++ src/app/review/step1/_components/achievement/Score.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useAtom, useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 
 import { getReviewId } from '@/apis/review/get';
 import { addScore } from '@/apis/review/post';
@@ -27,37 +28,40 @@ export const Score = () => {
   const [selectedScore, setSelectedScore] = useAtom(scoreAtom);
   const [reviewId, setReviewId] = useAtom(reviewIdAtom);
 
+  useEffect(() => {
+    (async () => {
+      if (!reviewId) {
+        // 이미 회고 id를 발급 받은 경우 - get으로 id, score 가져오기
+        const reviewIdResponse = await getReviewId(WeekInfo);
+
+        if (reviewIdResponse?.id) {
+          setReviewId(reviewIdResponse.id);
+          setSelectedScore(reviewIdResponse.like);
+        }
+      }
+    })();
+  }, [reviewId, setReviewId, setSelectedScore]);
+
   const onClickPickScore = async (count: number) => {
     setSelectedScore(count);
     setPageButtonStates((prev) => ({ ...prev, step1: true }));
 
-    if (!reviewId) {
-      // 이미 회고 id를 발급 받은 경우 - get으로 id 가져오기
-      const reviewIdResponse = await getReviewId(WeekInfo);
-
-      if (reviewIdResponse?.id) {
-        setReviewId(reviewIdResponse.id);
-        setSelectedScore(reviewIdResponse.like);
-        return;
-      }
-
-      // 처음 회고를 이용하는 경우 - post로 id 발급
-      try {
-        const response = await addScore({
-          weekNumber: {
-            year,
-            month,
-            week,
-          },
-          like: selectedScore,
-        });
-        setReviewId(response.id);
-      } catch (err) {
-        addToast({
-          message: '다시 시도해 주세요.',
-          iconType: 'error',
-        });
-      }
+    // 처음 회고를 이용하는 경우 - post로 id 발급
+    try {
+      const response = await addScore({
+        weekNumber: {
+          year,
+          month,
+          week,
+        },
+        like: selectedScore,
+      });
+      setReviewId(response.id);
+    } catch (err) {
+      addToast({
+        message: '다시 시도해 주세요.',
+        iconType: 'error',
+      });
     }
   };
 
