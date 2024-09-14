3675f3f1d538d7f7f973acbe718384e0f52bd6c2
msdio <dev.msdio@gmail.com>
2024-09-15 00:08:36 +0900

    ✨ feat(custom): 히스토리 페이지에서 메모 생성하는 기능 추가

--- src/app/(with-navigation)/history/_components/Category.tsx
+++ src/app/(with-navigation)/history/_components/Category.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-import { useAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
+import { AllMemosResponse, getAllMemos } from '@/apis/memo/get';
+import { addNewMemo } from '@/apis/memo/post';
 import PlusIcon from '@/components/icons/PlusIcon';
+import TextEditorModal from '@/components/modal/text-editor';
 import ToggleSwitch from '@/components/toggle-switch/ToggleSwitch';
 import { showOnlyBookmarkAtom } from '@/stores/bookmark/showOnlyBookmarkAtom';
+import { historyMemoListAtom, MemoList } from '@/stores/memos/memos';
+import { sortByWeek } from '@/types/sort';
 import { cn } from '@/utils/tailwind';
 
 import { dummy } from '../memo/_components/MemoHistory/dummy';
@@ -26,6 +32,47 @@ const Category = () => {
   const currentPathname = usePathname();
 
   const [showBookmarkOnly, setShowBookmarkOnly] = useAtom(showOnlyBookmarkAtom);
+  const setMemoList = useSetAtom(historyMemoListAtom);
+
+  const [openTextEditor, setOpenTextEditor] = useState(false);
+
+  const groupMemosByWeek = (receivedMemos: AllMemosResponse) => {
+    const groupedMemos = receivedMemos.contents.reduce(
+      (acc, cur) => {
+        const { year, month, week } = cur.weekNumber;
+
+        const item = {
+          id: cur.id,
+          content: cur.content,
+          isMarked: cur.isMarked,
+          updatedAt: cur.updatedAt,
+        };
+
+        const key = `${year}-${month}-${week}`;
+        if (acc[key]) {
+          acc[key].memos.push(item);
+        } else {
+          acc[key] = {
+            weekNumber: cur.weekNumber,
+            memos: [item],
+          };
+        }
+
+        return acc;
+      },
+      {} as Record<string, MemoList>,
+    );
+
+    return Object.values(groupedMemos).sort(sortByWeek);
+  };
+
+  const addMemo = async (content: string) => {
+    try {
+      await addNewMemo({ content, isMarked: false });
+    } catch (error) {
+      console.log('fail to add memo', error);
+    }
+  };
 
   return (
     <div className="flex items-center justify-between mb-5">
@@ -57,11 +104,30 @@ const Category = () => {
       </div>
 
       {currentPathname === '/history/memo' && (
-        <button type="button" className="button-primary button-medium">
+        <button
+          type="button"
+          className="button-primary button-medium"
+          onClick={() => setOpenTextEditor(true)}
+        >
           <PlusIcon size={20} stroke="#fff" />
           <p className="font-body-14 text-text-invert">추가</p>
         </button>
       )}
+
+      <TextEditorModal
+        isOpen={openTextEditor}
+        onDismiss={() => {
+          setOpenTextEditor(false);
+        }}
+        onSaveText={async (text) => {
+          await addMemo(text);
+          const response = await getAllMemos({});
+
+          const sorted = groupMemosByWeek(response);
+          setMemoList(sorted);
+        }}
+        value=""
+      />
     </div>
   );
 };
