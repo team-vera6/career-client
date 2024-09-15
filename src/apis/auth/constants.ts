ffcdcc2e81f7ad5ae41160f84ae0a9c4e278e706
MeaninGood <jsmeaningood@gmail.com>
2024-09-15 22:11:18 +0900

    ♻️ rfac(custom): auth 에러 enum 코드에 주석 추가

--- src/apis/auth/constants.ts
+++ src/apis/auth/constants.ts
@@ -1,6 +1,8 @@
 export const LOGIN_ERROR_MESSAGE: Record<string, string> = {
-  incorrect_password: '이메일 또는 비밀번호를 다시 확인해 주세요.',
+  invalid_request: '올바른 이메일 주소를 입력해 주세요.', // 회원가입, 로그인
+  already_exist_email: '이미 등록된 이메일입니다.', // 회원가입, 이메일 인증
+  invalid_password: '비밀번호를 다시 확인해 주세요.', // 회원가입
+  incorrect_password: '비밀번호를 다시 확인해 주세요.', // 회원가입
+  incorrect_credentials: '이메일 또는 비밀번호를 다시 확인해 주세요', // 로그인
   resource_not_found: '올바른 이메일 주소를 입력해 주세요.',
-  already_exist_email: '이미 등록된 이메일입니다.',
-  invalid_request: '올바른 이메일 주소를 입력해 주세요.',
 };
