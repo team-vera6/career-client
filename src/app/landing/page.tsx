e8892d98783bc031cdabd3aedb0bd169e8782048
msdio <dev.msdio@gmail.com>
2024-09-17 00:57:41 +0900

    ✨ feat(custom): 랜딩 페이지 텍스트 추가

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,26 +1,20 @@
-import Link from 'next/link';
-
+import Catchphrase from './_components/Catchphrase';
 import Footer from './_components/Footer';
 import Header from './_components/Header';
 import Images from './_components/Images';
-import Title from './_components/Title';
 
 export default function LandingPage() {
   return (
-    <main className="h-full min-h-screen min-w-[1024px] bg-surface-foreground px-[10rem] mobile:px-6 mobile:min-w-[360px]">
+    <main className="h-full min-h-screen min-w-[1024px] bg-surface-foreground">
       <Header />
 
-      <Title />
+      <section className="w-full flex flex-col items-center mt-20">
+        <Images />
 
-      <Link href="/dashboards" className="block w-fit">
-        <button className="button-primary button-large mb-20 mt-10 mobile:mt-7 mobile:mb-16 w-fit">
-          오픈베타 시작하기
-        </button>
-      </Link>
+        <Catchphrase />
 
-      <Images />
-
-      <Footer />
+        <Footer />
+      </section>
     </main>
   );
 }
