72467e11bf72db85a2b12b3dee61b621a8c855fb
msdio <dev.msdio@gmail.com>
2024-09-17 00:50:00 +0900

    ✨ feat(custom): 랜딩 페이지 헤더, 푸터 추가

--- src/app/landing/_components/Footer.tsx
+++ src/app/landing/_components/Footer.tsx
@@ -1,38 +1,40 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import PitstopLogoGray from '../../../assets/logos/LogoGray.png';
+import LogoGray from '@/assets/logos/LogoGray.png';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="w-[1120px] mt-20 mb-10">
+      <hr className="w-full border-line-normal mb-9" />
+
       <Image
-        src={PitstopLogoGray}
-        alt="gray pitstop logo on footer"
-        width={164}
-        height={25}
+        src={LogoGray}
+        alt="pitstop logo"
+        className="w-[164px] h-[25px] mb-1"
       />
 
-      <div className="flex items-center justify-between pb-9 mt-1 w-full mobile:block">
-        <p className="font-body-12 text-text-normal mobile:mb-5">
+      <div className="flex items-center justify-between">
+        <p className="font-body-12 text-text-normal">
           회고를 위한 가장 쉬운 선택
         </p>
 
-        <div className="flex items-center mobile:justify-between">
-          <div className="flex items-center gap-3 mr-4">
-            <Link href="/">
-              <p className="text-text-normal font-normal text-xs leading-[1.0625rem] underline">
-                이용약관
-              </p>
-            </Link>
-            <Link href="/">
-              <p className="text-text-normal font-normal text-xs leading-[1.0625rem] underline">
-                개인정보 처리방침
-              </p>
-            </Link>
-          </div>
-
-          <p className="font-body-13 text-text-normal">© 2024 PITSTOP</p>
+        <div className="flex items-center gap-3">
+          <Link
+            href="https://ancient-gladiolus-215.notion.site/PITSTOP-c91558f34f8b4e32bd2f307ce4215447?pvs=4"
+            target="_blank"
+          >
+            <p className="font-body-12 text-text-normal underline">이용약관</p>
+          </Link>
+          <Link
+            href="https://ancient-gladiolus-215.notion.site/PITSTOP-73e2c3e4dc87490aa6417b00ae060355?pvs=4"
+            target="_blank"
+          >
+            <p className="font-body-12 text-text-normal underline">
+              개인정보 처리방침
+            </p>
+          </Link>
+          <p className="font-body-12 text-text-normal">© 2024 PITSTOP</p>
         </div>
       </div>
     </footer>
