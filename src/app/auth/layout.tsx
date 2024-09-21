a552e6ddd7695d248acde575051fbfcc07dea52f
MeaninGood <jsmeaningood@gmail.com>
2024-09-22 01:57:33 +0900

    ✨ feat(custom): auth layout에서 logo 추가

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,5 @@
+import TextLogo from '@/components/icons/TextLogo';
+
 export default function AuthLayout({
   children,
 }: Readonly<{
@@ -6,6 +8,7 @@ export default function AuthLayout({
   return (
     <div className="w-full h-dvh flex justify-center pt-60 bg-surface-foreground">
       <div className="flex flex-col items-center">
+        <TextLogo />
         <main>{children}</main>
       </div>
     </div>
