80b711b3aa77a266217be61d3dddfe2bead8fa5f
msdio <dev.msdio@gmail.com>
2024-09-22 01:11:17 +0900

    🪛 fix(custom): 로그인 안 된 사용자가 root 경로로 진입 시 랜딩 페이지로 리다이렉트

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/dashboards',
+        destination: '/landing',
         permanent: true,
       },
       {
