6c3f1f5a1ea894c534c6ee16a4dcb519f1f56bde
msdio <dev.msdio@gmail.com>
2024-09-27 15:20:22 +0900

    ✨ feat(custom): 대시보드 페이지에 지표 클릭 시 해당하는 페이지로 이동

--- src/app/(with-navigation)/dashboards/_components/Metrics.tsx
+++ src/app/(with-navigation)/dashboards/_components/Metrics.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import FireIcon from '@/components/icons/FireIcon';
 import FlagIcon from '@/components/icons/FlagIcon';
 import YellowFolderIcon from '@/components/icons/YellowFolderIcon';
@@ -12,33 +14,39 @@ interface Props {
 const Metrics = ({ weekStreak, reviewCount, projectCount }: Props) => {
   return (
     <section className="flex gap-3 w-full mb-7">
-      <div className="w-[19.5rem] pl-10 h-20 rounded-xl bg-surface-foregroundOn flex items-center gap-4">
-        <FireIcon size={32} />
-        <p className="font-body-16 text-text-strong flex items-center">
-          <b className="font-chakra font-semibold italic mr-1.5 text-2xl">
-            {prefixZeros(weekStreak, 2)}
-          </b>
-          주 연속 회고
-        </p>
-      </div>
-      <div className="w-[19.5rem] pl-10 h-20 rounded-xl bg-surface-foregroundOn flex items-center gap-4">
-        <FlagIcon size={32} />
-        <p className="font-body-16 text-text-strong flex items-center">
-          <span className="font-chakra font-semibold italic mr-1.5 text-2xl">
-            {prefixZeros(reviewCount, 2)}
-          </span>
-          개 회고 기록
-        </p>
-      </div>
-      <div className="w-[19.5rem] pl-10 h-20 rounded-xl bg-surface-foregroundOn flex items-center gap-4">
-        <YellowFolderIcon size={32} />
-        <p className="font-body-16 text-text-strong flex items-center">
-          <span className="font-chakra font-semibold italic mr-1.5 text-2xl">
-            {prefixZeros(projectCount, 2)}
-          </span>
-          개 프로젝트
-        </p>
-      </div>
+      <Link href="/history">
+        <div className="w-[19.5rem] pl-10 h-20 rounded-xl bg-surface-foregroundOn flex items-center gap-4">
+          <FireIcon size={32} />
+          <p className="font-body-16 text-text-strong flex items-center">
+            <b className="font-chakra font-semibold italic mr-1.5 text-2xl">
+              {prefixZeros(weekStreak, 2)}
+            </b>
+            주 연속 회고
+          </p>
+        </div>
+      </Link>
+      <Link href="/history">
+        <div className="w-[19.5rem] pl-10 h-20 rounded-xl bg-surface-foregroundOn flex items-center gap-4">
+          <FlagIcon size={32} />
+          <p className="font-body-16 text-text-strong flex items-center">
+            <span className="font-chakra font-semibold italic mr-1.5 text-2xl">
+              {prefixZeros(reviewCount, 2)}
+            </span>
+            개 회고 기록
+          </p>
+        </div>
+      </Link>
+      <Link href="/project">
+        <div className="w-[19.5rem] pl-10 h-20 rounded-xl bg-surface-foregroundOn flex items-center gap-4">
+          <YellowFolderIcon size={32} />
+          <p className="font-body-16 text-text-strong flex items-center">
+            <span className="font-chakra font-semibold italic mr-1.5 text-2xl">
+              {prefixZeros(projectCount, 2)}
+            </span>
+            개 프로젝트
+          </p>
+        </div>
+      </Link>
     </section>
   );
 };
