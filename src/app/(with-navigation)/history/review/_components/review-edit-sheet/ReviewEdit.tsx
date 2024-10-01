7d4abaee256333bc5bb5c0a019c0f4a554592b15
msdio <dev.msdio@gmail.com>
2024-10-01 19:28:29 +0900

    ✨ feat(custom): 회고 수정 시트 개발

---
+++ src/app/(with-navigation)/history/review/_components/review-edit-sheet/ReviewEdit.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { LastWeekReviewItem } from '@/app/review/_components/last-week-review/LastWeekReviewItem';
+import HighlightCircleIcon from '@/components/icons/HighlightCircleIcon';
+import LowlightCircleIcon from '@/components/icons/LowlightCircleIcon';
+import DeletableInput from '@/components/inputs/deletable-input/DeletableInput';
+import { Highlight } from '@/types/highlight';
+import { Todo } from '@/types/todo';
+
+interface Props {
+  highlights: Highlight[];
+  lowlights: Highlight[];
+  completedTodos: Todo[];
+}
+
+export const ReviewEdit = ({
+  highlights,
+  lowlights,
+  completedTodos,
+}: Props) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2.5">
+        <div className="flex items-center gap-2">
+          <HighlightCircleIcon size={24} />
+          <p className="font-title-14 text-text-strong">하이라이트</p>
+        </div>
+        <div className="pl-8 flex flex-col gap-2">
+          {highlights.map((highlight) => (
+            <LastWeekReviewItem
+              key={`highlight-${highlight.id}`}
+              id={highlight.id}
+              project={highlight.project}
+              content={highlight.content}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <div className="flex items-center gap-2">
+          <LowlightCircleIcon size={24} />
+          <p className="font-title-14 text-text-strong">로우라이트</p>
+        </div>
+        <div className="pl-8 flex flex-col gap-2">
+          {lowlights.map((lowlight) => (
+            <LastWeekReviewItem
+              key={`lowlight-${lowlight.id}`}
+              id={lowlight.id}
+              project={lowlight.project}
+              content={lowlight.content}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <p className="font-title-14 text-text-strong">완료한 일</p>
+        <div className="pl-8 flex flex-col gap-2">
+          {completedTodos.map((todo) => (
+            <DeletableInput key={`todo-${todo.id}`} value={todo.content} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/(with-navigation)/history/review/_components/review-edit-sheet/ReviewEditSheet.tsx
@@ -0,0 +1,61 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import Score from '@/app/(with-navigation)/history/_components/review/Score';
+import { ReviewEdit } from '@/app/(with-navigation)/history/review/_components/review-edit-sheet/ReviewEdit';
+import RightActionSheetContainer from '@/components/action-sheets/Container';
+import { CurrentWeek } from '@/types/currentWeek';
+import { Highlight } from '@/types/highlight';
+import { Todo } from '@/types/todo';
+
+interface Props {
+  isOpen: boolean;
+  selectedReviewId: number;
+  setShowDetail: Dispatch<SetStateAction<boolean>>;
+  weekNumber: CurrentWeek;
+  highlights: Omit<Highlight, 'currentWeek'>[];
+  lowlights: Omit<Highlight, 'currentWeek'>[];
+  completedTodos: Todo[];
+}
+
+export const ReviewEditSheet = ({
+  isOpen,
+  selectedReviewId,
+  setShowDetail,
+  weekNumber,
+  highlights,
+  lowlights,
+  completedTodos,
+}: Props) => {
+  return (
+    <RightActionSheetContainer
+      isOpen={isOpen}
+      closeActionSheet={() => setShowDetail(false)}
+      buttons={[
+        {
+          text: '저장',
+          buttonStyle: 'primary',
+          onClick: () => console.log('저장'),
+        },
+      ]}
+    >
+      <section className="flex flex-col">
+        <div className="flex items-center justify-between mb-5.5">
+          <p className="font-head-28 text-text-strong">
+            {weekNumber.month}월 {weekNumber.week}주차
+          </p>
+
+          <div className="flex items-center gap-2">
+            <p className="font-body-14 text-text-strong">만족도</p>
+            <Score activeCount={5} className="bg-surface-foregroundOn" />
+          </div>
+        </div>
+
+        <ReviewEdit
+          highlights={highlights}
+          lowlights={lowlights}
+          completedTodos={completedTodos}
+        />
+      </section>
+    </RightActionSheetContainer>
+  );
+};
