04dc3d08e5aca6e8fd29df8146d4e3b325274cb6
MeaninGood <jsmeaningood@gmail.com>
2024-10-08 16:52:36 +0900

    ✨ feat(custom): 회고 step container 높이가 각 step에 맞게 변경되는 로직 추가

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAtomValue } from 'jotai';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { useReviewReset } from '@/hooks/useResetReview';
 import { cn } from '@/utils/tailwind';
@@ -22,6 +22,9 @@ const SIZE = 53.625; // width + gap 150px
 export default function ReviewPage() {
   const reviewStep = useAtomValue(reviewStepAtom);
 
+  const [containerHeight, setContainerHeight] = useState('100%');
+  const stepRefs = useRef<(HTMLDivElement | null)[]>([]);
+
   const resetReview = useReviewReset();
 
   useEffect(() => {
@@ -30,13 +33,34 @@ export default function ReviewPage() {
 
   const position = SIZE * (reviewStep - 1);
 
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const { height } = entry.contentRect;
+        setContainerHeight(`${height}px`);
+      }
+    });
+
+    if (stepRefs.current[reviewStep - 1]) {
+      resizeObserver.observe(stepRefs.current[reviewStep - 1]!);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [reviewStep]);
+
   return (
-    <div className="w-fit flex gap-[150px] overflow-hidden">
+    <div
+      className="w-fit flex gap-[150px] overflow-hidden"
+      style={{ height: containerHeight }}
+    >
       {steps.map((el, index) => (
         <div
           key={`Step-${index}`}
+          ref={(el) => (stepRefs.current[index] = el)}
           className={cn(
-            'w-[44.25rem] transition duration-500',
+            'w-[44.25rem] transition duration-500 h-fit',
             reviewStep - 1 === index ? 'opacity-100' : 'opacity-0',
           )}
           style={{
